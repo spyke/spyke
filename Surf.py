5962ffde92b18783c1fd40bea5b81ee812dc407d
Martin Spacek <git@mspacek.mm.st>
2007-03-12 18:10:21 -0700

    header, DRDBs, and RSFDs now parsing properly from .srf file, but this required hacks to skip over two segments of unexplained bytes (of length 156 and 2 respectively) in the DRDBs

--- Surf.py
+++ Surf.py
@@ -103,7 +103,7 @@ TYPE
 '''
 
 '''
-.fat format:
+Tim's .fat format:
     first the surf file header?
     for buffer in buffers:
         stream flag (PS or PC or VD or MS or MU) (2 bytes)
@@ -131,10 +131,17 @@ UFF_OWNER_LEN = 14
 UFF_FILEDESC_LEN = 64
 UFF_FH_USERAREA_LEN = UFF_FILEHEADER_LEN - 512 # 1536
 
+# Surf DRDB constants
+UFF_DRDB_BLOCK_LEN = 2048
+UFF_DRDB_NAME_LEN = 20
+UFF_DRDB_PAD_LEN = 16
+UFF_RSFD_PER_DRDB = 77
+UFF_DRDB_RSFD_NAME_LEN = 20
+
 class SurfFile(object): # or should I inherit from file?
     """Opens a .srf file and exposes all of its data as attribs.
     If no synonymous .parse file exists, parses the .srf file, stores parsing in a .parse file.
-    Stores as attribs: recording header (with electrode info), stimulus headers, high and low pass stream data, stimulus data, message data"""
+    Stores as attribs: srf header, electrode layout records, high and low pass stream data, stimulus display headers, stimulus digital vals, message data"""
     def __init__(self, fname='C:/data/Cat 15/81 - track 7c mseq16exps.srf'):
         self.f = file(fname, 'rb')
 
@@ -148,65 +155,147 @@ class SurfFile(object): # or should I inherit from file?
             u = cPickle.Unpickler(ff)
             fat = u.load()
             ff.close()
-            self.surfheader = fat.surfheader
+            self.fat = fat
             print 'Recovered fat from %r' % fatfname
         except: # .fat file doesn't exist, or something's wrong with it. Parse the .srf file
             print 'Parsing %r' % self.f.name
+            f = self.f # abbrev
+            f.seek(0) # make sure we're at the start of the srf file before trying to parse it
+
             # Parse the Surf header
-            f = self.f
-            f.seek(0) # make sure we're at the start of the srf file before trying to parse the srf header
-            sh = SurfHeader()
-            sh.FH_rec_type, = struct.unpack('B', f.read(1)) # must be 1
-            assert sh.FH_rec_type == 1
-            sh.FH_rec_type_ext, = struct.unpack('B', f.read(1)) # must be 0
-            assert sh.FH_rec_type_ext == 0
-            sh.UFF_name = f.read(UFF_NAME_LEN).rstrip('\x00') # must be 'UFF'
-            assert sh.UFF_name == 'UFF'
-            sh.UFF_major, = struct.unpack('B', f.read(1)) # major UFF ver
-            sh.UFF_minor, = struct.unpack('B', f.read(1)) # minor UFF ver
-            sh.FH_rec_len, = struct.unpack('H', f.read(2)) # FH record length in bytes
-            sh.DRDB_rec_len, = struct.unpack('H', f.read(2)) # DBRD record length in bytes
-            sh.bi_di_seeks, = struct.unpack('H', f.read(2)) # 2 bi-directional seeks format - what's word boolean's length?
-            sh.OS_name = f.read(UFF_OSNAME_LEN).rstrip('\x00') # OS name, ie "WINDOWS 2000"
-            sh.OS_major, = struct.unpack('B', f.read(1)) # OS major rev
-            sh.OS_minor, = struct.unpack('B', f.read(1)) # OS minor rev
-            sh.create = struct.unpack('H'*(18/2), f.read(18)) # creation time & date: Sec,Min,Hour,Day,Month,Year + 6 junk bytes
-            sh.append = struct.unpack('H'*(18/2), f.read(18)) # last append time & date: Sec,Min,Hour,Day,Month,Year + 6 junk bytes
-            sh.node = f.read(UFF_NODENAME_LEN).rstrip('\x00') # system node name - same as BDT
-            sh.device = f.read(UFF_DEVICE_LEN).rstrip('\x00') # device name - same as BDT
-            sh.path = f.read(UFF_PATH_LEN).rstrip('\x00') # path name
-            # this has some kinda problem in .srf file '81 - track 7c mseq16exps.srfcula'
-            sh.filename = f.read(UFF_FILENAME_LEN).rstrip('\x00') # original file name at creation
-            sh.pad = f.read(UFF_FH_PAD_LEN).replace('\x00', ' ') # pad area to bring uff area to 512
-            sh.app_info = f.read(UFF_APPINFO_LEN).rstrip('\x00') # application task name & version
-            sh.user_name = f.read(UFF_OWNER_LEN).rstrip('\x00') # user's name as owner of file
-            sh.file_desc = f.read(UFF_FILEDESC_LEN).rstrip('\x00') # description of file/exp
-            sh.user_area = struct.unpack('B'*UFF_FH_USERAREA_LEN, f.read(UFF_FH_USERAREA_LEN)) # additional user area
-            sh.bd_FH_rec_type, = struct.unpack('B', f.read(1)) # record type, must be 1 BIDIRECTIONAL SUPPORT
-            assert sh.bd_FH_rec_type == 1
-            sh.bd_FH_rec_type_ext, = struct.unpack('B', f.read(1)) # record type extension, must be 0 BIDIRECTIONAL SUPPORT
-            assert sh.bd_FH_rec_type_ext == 0
-            self.surfheader = sh
+            self.surfheader = SurfHeader(f)
+            self.surfheader.parse()
+
+            # Parse the DRDBs (Data Record Descriptor Blocks)
+            # starts at offset 2050
+            self.drdbs = []
+            while 1:
+                drdb = DRDB(f)
+                try:
+                    drdb.parse() # 158 too many extra bytes in file
+                    self.drdbs.append(drdb)
+                except DRDBError: # we've gone past the last DRDB
+                    f.seek(drdb.offset) # set file pointer back to where we were
+                    break
 
             # Now parse whatever comes next...
 
             # Create a fat for mapping between stream position and file position
             fat = SurfFat()
             fat.surfheader = self.surfheader
-            ff = file(fatfname, 'wb')
-            p = cPickle.Pickler(ff, protocol=-1) # make a Pickler, use most efficient (least readable) protocol
-            p.dump(fat) # pickle fat to file
-            ff.close()
+            fat.drdbs = self.drdbs
+            #ff = file(fatfname, 'wb')
+            #p = cPickle.Pickler(ff, protocol=-1) # make a Pickler, use most efficient (least readable) protocol
+            #p.dump(fat) # pickle fat to file
+            #ff.close()
         self.fat = fat
 
 class SurfHeader(object):
-    """Just a place to store srf file header fields"""
-    def __init__(self):
-        pass # init attribs here
+    """Surf file header.
+    Takes an open file, parses in from current file pointer position,
+    stores header fields as attibs"""
+    def __init__(self, f):
+        self.f = f
+    def parse(self):
+        f = self.f # abbrev
+        self.offset = f.tell()
+        self.FH_rec_type, = struct.unpack('B', f.read(1)) # must be 1
+        assert self.FH_rec_type == 1
+        self.FH_rec_type_ext, = struct.unpack('B', f.read(1)) # must be 0
+        assert self.FH_rec_type_ext == 0
+        self.UFF_name = f.read(UFF_NAME_LEN).rstrip('\x00') # must be 'UFF'
+        assert self.UFF_name == 'UFF'
+        self.UFF_major, = struct.unpack('B', f.read(1)) # major UFF ver
+        self.UFF_minor, = struct.unpack('B', f.read(1)) # minor UFF ver
+        self.FH_rec_len, = struct.unpack('H', f.read(2)) # FH record length in bytes
+        self.DRDB_rec_len, = struct.unpack('H', f.read(2)) # DBRD record length in bytes
+        self.bi_di_seeks, = struct.unpack('H', f.read(2)) # 2 bi-directional seeks format
+        self.OS_name = f.read(UFF_OSNAME_LEN).rstrip('\x00') # OS name, ie "WINDOWS 2000"
+        self.OS_major, = struct.unpack('B', f.read(1)) # OS major rev
+        self.OS_minor, = struct.unpack('B', f.read(1)) # OS minor rev
+        self.create = struct.unpack('H'*(18/2), f.read(18)) # creation time & date: Sec,Min,Hour,Day,Month,Year + 6 junk bytes
+        self.append = struct.unpack('H'*(18/2), f.read(18)) # last append time & date: Sec,Min,Hour,Day,Month,Year + 6 junk bytes
+        self.node = f.read(UFF_NODENAME_LEN).rstrip('\x00') # system node name - same as BDT
+        self.device = f.read(UFF_DEVICE_LEN).rstrip('\x00') # device name - same as BDT
+        self.path = f.read(UFF_PATH_LEN).rstrip('\x00') # path name
+        # this has some kinda problem in .srf file '81 - track 7c mseq16exps.srfcula'
+        self.filename = f.read(UFF_FILENAME_LEN).rstrip('\x00') # original file name at creation
+        self.pad = f.read(UFF_FH_PAD_LEN).replace('\x00', ' ') # pad area to bring uff area to 512
+        self.app_info = f.read(UFF_APPINFO_LEN).rstrip('\x00') # application task name & version
+        self.user_name = f.read(UFF_OWNER_LEN).rstrip('\x00') # user's name as owner of file
+        self.file_desc = f.read(UFF_FILEDESC_LEN).rstrip('\x00') # description of file/exp
+        assert f.tell() - self.offset == 512 # non user area of UFF header should be 512 bytes
+        self.user_area = struct.unpack('B'*UFF_FH_USERAREA_LEN, f.read(UFF_FH_USERAREA_LEN)) # additional user area
+        assert f.tell() - self.offset == UFF_FILEHEADER_LEN
+        # this is the end of the original UFF header I think, the next two fields seem to have been added on to the end by Tim:
+        self.bd_FH_rec_type, = struct.unpack('B', f.read(1)) # record type, must be 1 BIDIRECTIONAL SUPPORT
+        assert self.bd_FH_rec_type == 1
+        self.bd_FH_rec_type_ext, = struct.unpack('B', f.read(1)) # record type extension, must be 0 BIDIRECTIONAL SUPPORT
+        assert self.bd_FH_rec_type_ext == 0
+        self.length = f.tell() - self.offset # total length is 2050 bytes
+        assert self.length == 2050
+
+class DRDB(object):
+    """Data Record Descriptor Block, aka UFF_DATA_REC_DESC_BLOCK in SurfBawd.
+    Takes an open file, parses in from current file pointer position,
+    stores header fields as attibs"""
+    def __init__(self, f):
+        self.f = f
+    def parse(self):
+        f = self.f # abbrev
+        self.offset = f.tell()
+        self.DRDB_rec_type, = struct.unpack('B', f.read(1)) # record type; must be 2
+        if self.DRDB_rec_type != 2: # SurfBawd uses this to detect that it's passed the last DRDB, not exactly failsafe...
+            raise DRDBError
+        self.DRDB_rec_type_ext, = struct.unpack('B', f.read(1)) # record type extension
+        self.DR_rec_type = f.read(1) # Data Record type for DBRD 3-255, ie 'P' or 'V' or 'M', etc..
+        assert int(struct.unpack('B', self.DR_rec_type)[0]) in range(3, 256) # don't know quite why SurfBawd required these byte values, this is more than the normal set of ASCII chars
+        self.DR_rec_type_ext, = struct.unpack('B', f.read(1)) # Data Record type ext; ignored
+        self.DR_size, = struct.unpack('l', f.read(4)) # Data Record size in bytes, signed, -1 means dynamic
+        self.DR_name = f.read(UFF_DRDB_NAME_LEN).rstrip('\x00') # Data Record name
+        self.DR_num_fields, = struct.unpack('H', f.read(2)) # number of sub-fields in Data Record
+        self.DR_pad = struct.unpack('B'*UFF_DRDB_PAD_LEN, f.read(UFF_DRDB_PAD_LEN)) # pad bytes for expansion
+        self.DR_subfields = [] # sub fields desc. RSFD = Record Subfield Descriptor
+        for rsfdi in range(UFF_RSFD_PER_DRDB):
+            rsfd = RSFD(f)
+            rsfd.parse()
+            self.DR_subfields.append(rsfd)
+        assert f.tell() - self.offset == UFF_DRDB_BLOCK_LEN
+        # this is the end of the original DRDB I think, the next two fields seem to have been added on to the end by Tim:
+        f.seek(156, 1) # hack to skip past unexplained extra 156 bytes (happens to be 6*RSFD.length)
+        self.bd_DRDB_rec_type, = struct.unpack('B', f.read(1)) # record type; must be 2 BIDIRECTIONAL SUPPORT
+        assert self.bd_DRDB_rec_type == 2
+        self.bd_DRDB_rec_type_ext, = struct.unpack('B', f.read(1)) # record type extension; must be 0 BIDIRECTIONAL SUPPORT
+        assert self.bd_DRDB_rec_type_ext == 0
+        f.seek(2, 1) # hack to skip past unexplained extra 2 bytes, sometimes they're 0s, sometimes not
+        self.length = f.tell() - self.offset # total length should be 2050 bytes, but with above skip hacks, it's 2208 bytes
+        assert self.length == 2208
+
+class DRDBError(ValueError):
+    """Used to indicate when you've passed the last DRDB at the start of the .srf file"""
+    pass
+
+class RSFD(object):
+    """Record Subfield Descriptor for Data Record Descriptor Block
+    Takes an open file, parses in from current file pointer position,
+    stores header fields as attibs"""
+    def __init__(self, f):
+        self.f = f
+    def parse(self):
+        f = self.f # abbrev
+        self.offset = f.tell()
+        self.subfield_name = f.read(UFF_DRDB_RSFD_NAME_LEN).rstrip('\x00') # DRDB subfield name
+        self.subfield_data_type, = struct.unpack('H', f.read(2)) # underlying data type
+        self.subfield_size, = struct.unpack('l', f.read(4)) # sub field size in bytes, signed
+        self.length = f.tell() - self.offset
+        assert self.length == 26
 
 class SurfFat(object): # stores all the stuff to be pickled into a .fat and then unpickled as saved parse info
     def __init__(self):
-        self.surfheader = None # srf header
+        pass
+        # initing these to None ain't really necessary:
+        #self.surfheader = None # srf header
+        #self.drdbs = None # list of DRDBs
         # fill in other stuff here
 
 class SurfStream(object): # or should I inherit from np.ndarray?
