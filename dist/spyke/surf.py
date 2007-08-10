ed70883a79870d9ec24c04a5693bd98b74eecc70
Reza Lotun <rlotun@gmail.com>
2007-08-09 21:36:58 -0700

    Various code cleanups for spyke.surf - note new lowercasing of module names. Factored out stream interface code into spyke.stream - there should be a strict seperation between class which model the actual file and its indices, and the interface to that file.

--- dist/spyke/surf.py
+++ dist/spyke/surf.py
@@ -1,4 +1,4 @@
-"""Handles parsing and streaming of Surf-generated .srf files"""
+"""Handles parsing of Surf-generated .srf files"""
 
 __authors__ = ['Martin Spacek', 'Reza Lotun']
 
@@ -77,17 +77,24 @@ class File(object):
     """
     def __init__(self, name=DEFAULTSRFFNAME):
         self.name = name
+        self.fileSize = os.stat(self.name)[6]
         self.open()
         self.parsefname = os.path.splitext(self.f.name)[0] + '.parse'
         self.sections = []
+        self.sectionSummary = {}
 
     def __str__(self):
         if len(self.sections) == 0:
             return self.name + ' has not been parsed yet.'
         else:
             # we assume the file has been parsed correctly
-            for sec in self.sections:
-                print sec.id, len(sec)
+            if '_cached_' not in self.sectionSummary:
+                self.sectionSummary['_cached_'] = True
+
+                for sec in self.sections:
+                    num = self.sectionSummary.setdefault(sec.id, 0) + 1
+                    self.sectionSummary[sec.id] = num
+            return str(self.sectionSummary)
 
     def open(self):
         """ Open the .srf file """
@@ -213,7 +220,9 @@ class File(object):
                     'VA'    :   AnalogSingleValRecord,
                   }
 
+        i = 0
         while True:
+            i += 1
             # returns an empty string when EOF is reached
             flag = f.read(2).replace('\0', '')
             if flag == '':
@@ -239,9 +248,12 @@ class File(object):
                 #raise ValueError, \
                 #        'Unexpected flag %r at offset %d' % (flag, f.tell())
 
+            self.percentParsed = f.tell() * 100 / self.fileSize
+
     def _connectRecords(self):
         # Connect the appropriate probe layout to each high
         # and lowpass record
+
         print 'Connecting probe layouts to waveform records'
         for record in self.highpassrecords:
             record.layout = self.layoutrecords[record.Probe]
@@ -262,15 +274,16 @@ class File(object):
         # with the same timestamp
 
         rtsis = np.concatenate([[0], rtsis+1, [len(rts)]])
-        for rtsii in range(1, len(rtsis)): # start with the second rtsi
+        self.lowpassmultichanrecords = []
+        for rtsii in xrange(1, len(rtsis)): # start with the second rtsi
             lo = rtsis[rtsii-1]
             hi = rtsis[rtsii]
             lpass = LowPassMultiChanRecord(self.lowpassrecords[lo:hi])
             self.lowpassmultichanrecords.append(lpass)
 
-        self.highpassstream = Stream(records=self.highpassrecords,
-                                        sampfreq=DEFAULTINTERPSAMPFREQ)
-        self.lowpassstream = Stream(records=self.lowpassrecords)
+        #self.highpassstream = Stream(records=self.highpassrecords,
+        #                                sampfreq=DEFAULTINTERPSAMPFREQ)
+        #self.lowpassstream = Stream(records=self.lowpassrecords)
 
     def serialize(self):
         """ Creates a Fat object, saves all the parsed headers and records to
@@ -1002,190 +1015,6 @@ class DigitalSValRecord(object):
                 struct.unpack('QQHHI', f.read(24))
 
 
-class Stream(object):
-    """ Streaming object. Maps from timestamps to record index of stream data to
-    retrieve the approriate range of waveform data from disk.
-    """
-    def __init__(self, records=None, sampfreq=None):
-        """ Takes a sorted temporal (not necessarily evenly-spaced, due to
-        pauses in recording) sequence of ContinuousRecords: either
-        HighPassRecords or LowPassMultiChanRecords
-        """
-        self.records = records
-
-        # array of record timestamps
-        self.rts = np.asarray([record.TimeStamp for record in self.records])
-
-        # if no sampfreq passed in, use sampfreq of the raw data
-        self.sampfreq = sampfreq or records[0].layout.sampfreqperchan
-
-    def __len__(self):
-        """ Total number of timepoints? Length in time? Interp'd or raw? """
-        raise NotImplementedError()
-
-    def __getitem__(self, key, endinclusive=False):
-        """ Called when Stream object is indexed into using [] or with a slice
-        object, indicating start and end timepoints in us. Returns the
-        corresponding WaveForm object, which has as its attribs the 2D
-        multichannel waveform array as well as the timepoints,
-        potentially spanning multiple ContinuousRecords
-        """
-
-        # for now, accept only slice objects as keys
-        assert key.__class__ == slice
-
-        # find the first and last records corresponding to the slice. If the
-        # start of the slice matches a record's timestamp, start with that
-        # record. If the end of the slice matches a record's timestamp, end
-        # with that record (even though you'll only potentially use the one
-        # timepoint from that record, depending on the value of 'endinclusive')
-        lorec, hirec = self.rts.searchsorted([key.start, key.stop],
-                                                                side='right')
-
-        # we always want to get back at least 1 record (ie records[0:1]). When
-        # slicing, we need to do lower bounds checking (don't go less than 0),
-        # but not upper bounds checking
-        cutrecords = self.records[max(lorec-1, 0):max(hirec, 1)]
-        for record in cutrecords:
-            try:
-                record.waveform
-            except AttributeError:
-                # to save time, only load the waveform if not already loaded
-                record.load()
-
-        # join all waveforms, returns a copy
-        data = np.concatenate([record.waveform for record in cutrecords],
-                                axis=1)
-        try:
-
-            # all highpass records should be using the same layout,
-            # use tres from the first one
-            tres = cutrecords[0].layout.tres
-
-        except AttributeError:
-            # records are lowpassmulti
-            tres = cutrecords[0].tres
-
-        # build up waveform timepoints, taking into account any time gaps in
-        # between records due to pauses in recording
-        ts = []
-        for record in cutrecords:
-            tstart = record.TimeStamp
-            # number of timepoints (columns) in this record's waveform
-            nt = record.waveform.shape[-1]
-            ts.extend(range(tstart, tstart + nt*tres, tres))
-
-            #del record.waveform
-            # save memory by unloading waveform data from records that
-            # aren't needed anymore
-        ts = np.asarray(ts)
-        lo, hi = ts.searchsorted([key.start, key.stop])
-        data = data[:, lo:hi+endinclusive]
-        ts = ts[lo:hi+endinclusive]
-
-        # interp and s+h correct here
-        data, ts = self.interp(data, ts, self.sampfreq)
-
-        # return a WaveForm object
-        return WaveForm(data=data, ts=ts, sampfreq=self.sampfreq)
-
-    def interp(self, data, ts, sampfreq=None, kind='nyquist'):
-        """ Returns interpolated and sample-and-hold corrected data and
-        timepoints, at the given sample frequency
-        """
-        if kind == 'nyquist':
-            # do Nyquist interpolation and S+H correction here
-            # find a scipy function that'll do Nyquist interpolation?
-            # TODO: Implement this!
-            return data, ts
-        else:
-            raise ValueError, 'Unknown kind of interpolation %r' % kind
-
-    def plot(self, chanis=None, trange=None):
-        """ Creates a simple matplotlib plot of the specified chanis over
-        trange
-        """
-        # wouldn't otherwise need this
-        import pylab as pl
-        from pylab import get_current_fig_manager as gcfm
-
-        try:
-            # see if neuropy is available
-            from neuropy.Core import lastcmd, neuropyScalarFormatter, \
-                                                        neuropyAutoLocator
-        except ImportError:
-            pass
-
-        if chanis == None:
-            # all high pass records should have the same chanlist
-            if self.records[0].__class__ == HighPassRecord:
-                chanis = self.records[0].layout.chanlist
-                # same goes for lowpassmultichanrecords, each has its own
-                # set of chanis, derived previously from multiple
-                # single layout.chanlists
-            elif self.records[0].__class__ == LowPassMultiChanRecord:
-                chanis = self.records[0].chanis
-            else:
-                raise ValueError, 'unknown record type %s in self.records' % \
-                                                    self.records[0].__class__
-        nchans = len(chanis)
-
-        if trange == None:
-            trange = (self.rts[0], self.rts[0]+100000)
-
-        # make a waveform object
-        wf = self[trange[0]:trange[1]]
-        figheight = 1.25+0.2*nchans
-        self.f = pl.figure(figsize=(16, figheight))
-        self.a = self.f.add_subplot(111)
-
-        try:
-            gcfm().frame.SetTitle(lastcmd())
-        except NameError:
-            pass
-        except AttributeError:
-            pass
-
-        try:
-
-            # better behaved tick label formatter
-            self.formatter = neuropyScalarFormatter()
-
-            # use a thousands separator
-            self.formatter.thousandsSep = ','
-
-            # better behaved tick locator
-            self.a.xaxis.set_major_locator(neuropyAutoLocator())
-            self.a.xaxis.set_major_formatter(self.formatter)
-
-        except NameError:
-            pass
-        for chanii, chani in enumerate(chanis):
-            # upcast to int32 to prevent int16 overflow
-            self.a.plot(wf.ts/1e3,
-                    (np.int32(wf.data[chanii])-2048+500*chani)/500., '-',
-                                                            label=str(chani))
-        #self.a.legend()
-        self.a.set_xlabel('time (ms)')
-        self.a.set_ylabel('channel id')
-
-        # assumes chanis are sorted
-        self.a.set_ylim(chanis[0]-1, chanis[-1]+1)
-        bottominches = 0.75
-        heightinches = 0.15+0.2*nchans
-        bottom = bottominches / figheight
-        height = heightinches / figheight
-        self.a.set_position([0.035, bottom, 0.94, height])
-        pl.show()
-
-
-class WaveForm(object):
-    """ Waveform object, has data, timestamps and sample frequency attribs """
-    def __init__(self, data=None, ts=None, sampfreq=None):
-        self.data = data
-        # array of timestamps, one for each sample (column) in self.data
-        self.ts = ts
-        self.sampfreq = sampfreq
 
 
 class Fat(object):
@@ -1196,14 +1025,6 @@ class Fat(object):
     pass
 
 '''
-class HighPassStream(Stream): # or call this SpikeStream?
-    def __init__(self, records=None, sampfreq=DEFAULTINTERPSAMPFREQ):
-        super(HighPassStream, self).__init__(records=records, sampfreq=sampfreq)
-
-
-class LowPassStream(Stream): # or call this LFPStream?
-    def __init__(self, records=None), sampfreq=None:
-        super(LowPassStream, self).__init__(records=records, sampfreq=sampfreq)
 '''
 
 def causalorder(records):
