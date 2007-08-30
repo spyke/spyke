94f78a95db38088aeb9d428779e8216f0c874007
Reza Lotun <rlotun@gmail.com>
2007-08-30 09:25:50 -0700

    major reorganization

--- demo/eventwin.py
+++ demo/eventwin.py
@@ -13,15 +13,18 @@ import wx
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
 from matplotlib.figure import Figure
 import matplotlib.numerix as nx
+import numpy as np
 
 import spyke.surf
 import spyke.stream
+from spyke.plot import EventWindow, ChartWindow
 
 class Demo(wx.App):
     def OnInit(self):
         self.frame = panel = EventWin(None, -1, 'Events', size=(500,600))
-        #self.frame = panel = EventWin(None, -1, 'Events')
+        #self.frame = panel = ChartWin(None, -1, 'Data', size=(500,600))
         self.frame.Show(True)
+        #self.frame2.Show(True)
         return True
 
 class EventWin(wx.Frame):
@@ -29,143 +32,41 @@ class EventWin(wx.Frame):
         #self.filename = '/media/windows/Documents and Settings/Reza ' \
         #                'Lotun/Desktop/Surfdata/' \
         #                '87 - track 7c spontaneous craziness.srf'
-        self.filename = '../data/smallSurf'
         wx.Frame.__init__(self, parent, id, title, **kwds)
-        self.plotPanel = FigureCanvasWxAgg(self, -1, Figure())
-        #self.plotPanel.figure.set_edgecolor('white')
-        self.plotPanel.figure.set_facecolor('black')
-        self.plotPanel.SetBackgroundColour(wx.BLACK)
+        self.plotPanel = EventWindow(self)
 
         self.timer = wx.Timer(self)
-        self.Bind(wx.EVT_TIMER, self.onTimerEvent, self.timer)
+        self.Bind(wx.EVT_TIMER, self.plotPanel.onTimerEvent, self.timer)
         self.data = None
         self.points = []
         self.selectionPoints = []
         self.borderAxes = None
 
-        self.channels = {}
-        self._initSpyke()
         self.timer.Start(100)
-
-    def _initSpyke(self):
-        self.datafile = spyke.surf.File(self.filename)
-        self.datafile.parse()
-        self.colours = []
-        #col = ['r', 'g', 'b']
-        #for i in xrange(54):
-        #    self.colours.append(col[i % 3])
-        self.dstream = spyke.stream.Stream(self.datafile.highpassrecords)
-        self.curr = self.dstream.records[0].TimeStamp
-        self.incr = 1000
-
-        self.init_plot()
 
     def onEraseBackground(self, evt):
         # prevent redraw flicker
         pass
 
-    def update(self):
-        self.window = self.dstream[self.curr:self.curr+self.incr]
-        self.curr += self.incr
-        for chan in self.channels:
-            self.channels[chan].set_ydata(self.window.data[chan])
-            self.axes[chan].set_ylim((1650, 2400))
-            #print self.axes[chan].get_ylim()
-            #print self.axes[chan].get_autoscale_on()
-        self.plotPanel.draw(True)
+class ChartWin(wx.Frame):
+    def __init__(self, parent, id, title, **kwds):
+        #self.filename = '/media/windows/Documents and Settings/Reza ' \
+        #                'Lotun/Desktop/Surfdata/' \
+        #                '87 - track 7c spontaneous craziness.srf'
+        wx.Frame.__init__(self, parent, id, title, **kwds)
+        self.plotPanel = ChartWindow(self)
 
-    def onTimerEvent(self, evt):
-        self.update()
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.plotPanel.onTimerEvent, self.timer)
+        self.data = None
+        self.points = []
+        self.selectionPoints = []
+        self.borderAxes = None
 
-    def init_plot(self):
+        self.timer.Start(100)
 
-        ############
-        #
-        num = 54
-        spacing = [0.00, 0.00, 0.00, 0.00]
-        #offset = 0.02
-        #overlap = 0.02
-        offset = 0.0
-        overlap = 0.0
-        #
-        #############
-
-
-        fig = self.plotPanel.figure
-        #fig.clear()
-        horizMargin, vertMargin, hSep, vSep = spacing
-        width = (1 - 2 * horizMargin - hSep) / 2
-        n = num / 2
-        height = (1 - 2 * vertMargin - (n - 1) * vSep) / n
-        bot = vertMargin
-        chan = 0
-
-        self.window = self.dstream[self.curr:self.curr+self.incr]
-        self.curr += self.incr
-        self.axes = {}
-        for i in range(num // 2):
-            sp=[horizMargin, bot - offset, width, height]
-            a = fig.add_axes(sp, axisbg='y', frameon=False, alpha=1.)
-            #a.set_ylim((0, MAXY))
-            #a.plot(self.window.ts,
-            #       self.window.data[chan],
-            #       self.colours[chan],
-            #       antialiased=False,
-            #       linewidth=0.05)
-            #a.set_aspect('equal')
-            #a.set_adjustable('box')
-            a.plot(self.window.data[chan],
-                   'g',
-                   antialiased=False,
-                   linewidth=0.05,)
-                   #scaley=False)
-            #a.set_autoscale_on(False)
-            self.axes[chan] = a
-            self.channels[chan] = a.get_lines()[0]
-            a.grid(True)
-            a.set_xticks([])
-            a.set_yticks([])
-
-            # next channel
-            chan += 1
-
-            # XXX: duplicate code here - find a graceful way to code this
-            # once
-            sp=[horizMargin + width + hSep - overlap,
-                                        bot, width, height]
-
-
-            a = fig.add_axes(sp, axisbg='y', frameon=False, alpha=1.)
-            a.set_adjustable('box')
-            #a.set_aspect('equal')
-            #a.set_ylim((0, MAXY))
-            #a.plot(self.window.ts,
-            #       self.window.data[chan],
-            #       self.colours[chan],
-            #       antialiased=False,
-            #       linewidth=0.05)
-            a.plot(self.window.data[chan],
-                   'g',
-                   antialiased=False,
-                   linewidth=0.05,)
-                   #scaley=False)
-            #a.set_autoscale_on(False)
-            self.channels[chan] = a.get_lines()[0]
-            self.axes[chan] = a
-            a.grid(True)
-            a.set_xticks([])
-            a.set_yticks([])
-            bot += height + vSep
-
-            # next channel
-            chan += 1
-
-        if self.borderAxes:
-            a = fig.sca(self.borderAxes)
-            a.set_frame_on(True)
-
-        # redraw the disply
-        self.plotPanel.draw(True)
-
+    def onEraseBackground(self, evt):
+        # prevent redraw flicker
+        pass
 app = Demo()
 app.MainLoop()
--- dist/spyke/plot.py
+++ dist/spyke/plot.py
@@ -14,7 +14,7 @@ import matplotlib.numerix as nx
 import spyke.surf
 import spyke.stream
 
-def calc_spacings(layout):
+def calc_spacings(layout, figh, figw):
     """ Map from polytrode locations given as (x, y) coordinates
     into position information for the spike plots, which are stored
     as a list of four values [l, b, w, h]. To illustrate this, consider
@@ -27,27 +27,243 @@ def calc_spacings(layout):
           |        | loc_i (h)
           |        |       |
           |        +-------+
-          |            ^ 
-          |            | (b) 
-          |            v 
+          |            ^
+          |            | (b)
+          |            v
           +------------------------------+
          (1,0)                          (1,1)
     """
+
+    xcoords = [x for x, y in layout.itervalues()]
+    ycoords = [y for x, y in layout.itervalues()]
+    xmin, xmax = min(xcoords), max(xcoords)
+    ymin, ymax = min(ycoords), max(ycoords)
+    boxwidth, boxheight = xmax - xmin, ymax - ymin
+    numchannels = len(xcoords)
+    columns = len(set(xcoords))
+    num_per_column = numchannels / columns
+
+    ########
+    rl_margin, tb_margin, hsep, vsep = 0.01, 0.01, 0.01, 0.01
+    w = (1 - 2 * rl_margin - hsep) / columns
+    h = (1 - 2 * tb_margin - (num_per_column - 1) * vsep) / num_per_column
+    ########
+
+    plotwidth = w * boxwidth
+    plotheight = h * boxheight
+    rl_margin_l = rl_margin * box_width
+    tb_margin_l = tb_margin * box_height
+    hsep_l = hsep * boxwidth
+    vsep_l = vsep * boxheigth
+
+    new_box_width = plotwidth * columns + hsep_l + 2 * rl_margin_l
+    new_box_height = plotheight * num_per_column  \
+                        * (num_per_column -1) * vsep_l \
+                        * tb_margin_l * 2
+
+    pos = {}
+    for chan, loc in layout.iteritems():
+        x, y = loc
+
+        # XXX: make this better
+        if x == xmin:
+            col = 1
+        elif x == xmax:
+            col = numcols
+        elif x < xmax:
+            col = numcols - 1
+
+        #x = x - xmin + rl_margin_l + plotwidth / 2
+        #y = y - ymin + tb_margin_l +
 
 class PlotPanel(FigureCanvasWxAgg):
     """ A generic set of spyke plots. Meant to be a superclass of specific
     implementations of a plot panel (e.g. ChartPanel, EventPanel, etc.)
     """
     def __init__(self, frame):
+        self.filename = '../data/smallSurf'
         FigureCanvasWxAgg.__init__(self, frame, -1, Figure())
-        self.setColours()
-        self.channels{}
 
-    def setColours(self):
+        self.num_channels = 54 # XXX: hack for now
+        self.pos = {}
+        self.channels = {}
+        self.dstream = {}
+
+        self._initSpyke()
+
+        self.set_colours()
+        self.set_channel_layout()
+        self.init_plot()
+
+
+    def set_colours(self):
         self.figure.set_facecolor('black')
         self.SetBackgroundColour(wx.BLACK)
 
-    def init_plot(self):
+    def set_channel_layout(self):
         pass
 
+    def update(self):
+        self.window = self.dstream[self.curr:self.curr+self.incr]
+        self.curr += self.incr
+        for chan in self.channels:
+            #data = self.window.data[chan] - 2048+500*chan / 500.
+            #self.channels[chan].set_ydata(data)
+            self.channels[chan].set_ydata(self.window.data[chan])
+            self.axes[chan].set_ylim((1650, 2400))
+            #print self.axes[chan].get_ylim()
+            #print self.axes[chan].get_autoscale_on()
+        self.draw(True)
+
+    def onTimerEvent(self, evt):
+        self.update()
+
+    def _initSpyke(self):
+        self.datafile = spyke.surf.File(self.filename)
+        self.datafile.parse()
+        self.colours = []
+        col = ['g', 'g', 'g']  # XXX: clean this up!
+        for i in xrange(54):
+            self.colours.append(col[i % 3])
+        self.dstream = spyke.stream.Stream(self.datafile.highpassrecords)
+        self.curr = self.dstream.records[0].TimeStamp
+        self.incr = 1000
+
+    def init_plot(self):
+        self.window = self.dstream[self.curr:self.curr+self.incr]
+        self.curr += self.incr
+        self.axes = {}
+
+        for chan, sp in self.pos.iteritems():
+            a = self.figure.add_axes(sp, axisbg='y', frameon=False, alpha=1.)
+            #a.set_ylim((0, MAXY))
+            #a.plot(self.window.ts,
+            #       self.window.data[chan],
+            #       self.colours[chan],
+            #       antialiased=False,
+            #       linewidth=0.05)
+            #a.set_aspect('equal')
+            #a.set_adjustable('box')
+            a.plot(self.window.ts,
+                   self.window.data[chan],
+                   self.colours[chan],
+                   antialiased=False,
+                   linewidth=0.05,)
+                   #scaley=False)
+            #a.set_autoscale_on(False)
+            self.axes[chan] = a
+            self.channels[chan] = a.get_lines()[0]
+            a.grid(True)
+            a.set_xticks([])
+            a.set_yticks([])
+
+        #if self.borderAxes:
+        #    a = self.figure.sca(self.borderAxes)
+        #    a.set_frame_on(True)
+
+        # redraw the disply
+        self.draw(True)
+
+class ChartWindow(PlotPanel):
+    """ Chart window widget. Presents all channels layout out vertically. """
+
+    def set_channel_layout(self):
+        num = 54 # XXX: clean up!
+        hMargin = 0.05
+        vMargin = 0.05
+        vSpace = 0.001
+
+        width = 1 - 2 * hMargin
+        height = (1 - 2 * vMargin - (num - 1) * vSpace) / num
+        bot = vMargin
+
+        for i in xrange(num):
+            self.pos[i] = [hMargin, bot, width, height]
+            bot += height + vSpace
+
+    def _set_channel_layout(self):
+        ############
+        #
+        num = 54
+        spacing = [0.00, 0.00, 0.00, 0.00]
+        #offset = 0.02
+        #overlap = 0.02
+        offset = 0.0
+        overlap = 0.0
+        #
+        #############
+
+        horizMargin, vertMargin, hSep, vSep = spacing
+        width = (1 - 2 * horizMargin - hSep) / 2
+        n = num / 2
+        height = (1 - 2 * vertMargin - (n - 1) * vSep) / n
+        bot = vertMargin
+        chan = 0
+
+        for i in range(num // 2):
+            self.pos[chan]=[horizMargin, bot - offset, width, height]
+            # next channel
+            chan += 1
+
+            self.pos[chan]=[horizMargin + width + hSep - overlap,
+                                        bot, width, height]
+
+            bot += height + vSep
+            # next channel
+            chan += 1
+
+class EventWindow(PlotPanel):
+    """ Event window widget. Presents all channels layed out according
+    to the passed in layout. """
+
+    def set_colours(self):
+        self.figure.set_facecolor('black')
+        self.SetBackgroundColour(wx.BLACK)
+        self.colours = []
+        col = ['r', 'g', 'b']
+        for i in xrange(54):
+            self.colours.append(col[i % 3])
+
+    def set_channel_layout(self):
+        # XXX: working on mapping actually layout we'll play with this
+        # hack for now
+        ############
+        #
+        num = 54
+        spacing = [0.00, 0.00, 0.00, 0.00]
+        #offset = 0.02
+        #overlap = 0.02
+        offset = 0.0
+        overlap = 0.0
+        #
+        #############
+
+        horizMargin, vertMargin, hSep, vSep = spacing
+        width = (1 - 2 * horizMargin - hSep) / 2
+        n = num / 2
+        height = (1 - 2 * vertMargin - (n - 1) * vSep) / n
+        bot = vertMargin
+        chan = 0
+
+        self.window = self.dstream[self.curr:self.curr+self.incr]
+        self.curr += self.incr
+        self.axes = {}
+        for i in range(num // 2):
+            self.pos[chan]=[horizMargin, bot - offset, width, height]
+            # next channel
+            chan += 1
+
+            self.pos[chan]=[horizMargin + width + hSep - overlap,
+                                        bot, width, height]
+
+            bot += height + vSep
+            # next channel
+            chan += 1
+
+class SortingWindow(EventWindow):
+    """ Sorting window widget. Presents all channels layed out according
+    to the passed in layout. Also allows overplotting and some user
+    interaction
+    """
+    pass
 
