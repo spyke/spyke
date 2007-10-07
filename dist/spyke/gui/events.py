a07310514755790714a4f898dffc20b7b367fffe
Reza Lotun <rlotun@gmail.com>
2007-10-07 14:22:18 -0700

    forgot to add this

---
+++ dist/spyke/gui/events.py
@@ -0,0 +1,15 @@
+""" spyke.gui.events 
+
+Custom wx events used for communication between spyke gui components
+"""
+import wx
+
+class PlotEvent(wx.PyCommandEvent):
+    def __init__(self, evtType, id):
+        wx.PyCommandEvent.__init__(self, evtType, id)
+        self.plot = None
+        self.remove = None
+
+myEVT_PLOT = wx.NewEventType()
+EVT_PLOT = wx.PyEventBinder(myEVT_PLOT, 1)
+
