b7eba7e65f318aad1dc8ab966766dfd693ce8c19
Martin Spacek <git@mspacek.mm.st>
2009-08-18 17:41:17 -0700

    for ellipsoid_bounds.py demo, plot points inside and outside the ellipse with a different colour, to confirm that nothing funny is being done to the original set of random points

--- demo/ellipsoid_bounds.py
+++ demo/ellipsoid_bounds.py
@@ -28,16 +28,17 @@ def Rz(t):
 #def R(tx, ty, tz):
 #    return Rz(tz)*Ry(ty)*Rx(tx)
 
-
+# translation params
 x0 = 50
 y0 = 1023
 z0 = -23
 
+# scaling params
 A = 17.2
 B = 99
 C = 43.2
 
-# mayavi (tvtk actually) rotates axes in Z, X, Y order, for some unknown reason
+# orientation params
 txdeg = 15.23
 tydeg = 187.7
 tzdeg = -420
@@ -49,6 +50,7 @@ tz = tzdeg * np.pi / 180
 #RxRy = np.dot(Rx(tx), Ry(ty))
 #RxRyRz = np.dot(RxRy, Rz(tz))
 
+# mayavi (tvtk actually) rotates axes in Z, X, Y order, for some unknown reason
 RzRx = np.dot(Rz(tz), Rx(tx))
 RzRxRy = np.dot(RzRx, Ry(ty))
 
@@ -60,8 +62,10 @@ RzRxRy = np.dot(RzRx, Ry(ty))
 
 #R = Rz(tz)*Ry(ty)*Rx(tx) # they need to be matrices for this to work, yet plotted result is still wrong
 
-# draw an ellipse
-f = mlab.figure()
+# create a figure
+f = mlab.figure(bgcolor=(0, 0, 0))
+
+# draw an ellipsoid
 engine = f.parent
 f.scene.disable_render = True # for speed
 source = ParametricSurface()
@@ -77,16 +81,17 @@ actor.property.color = 1, 0, 0
 actor.mapper.scalar_visibility = False
 actor.property.backface_culling = True # gets rid of weird rendering artifact when opacity is < 1
 #actor.property.frontface_culling = True
-actor.actor.orientation = txdeg, tydeg, tzdeg #np.random.rand(3) * 360 # in degrees
-#actor.actor.origin = np.random.rand(3)
+actor.actor.orientation = txdeg, tydeg, tzdeg
+#actor.actor.origin = 0, 0, 0
 actor.actor.position = x0, y0, z0
 actor.actor.scale = A, B, C
 
 # set up cube of points centerd on and engulfing the ellipsoid
-p = 2 * np.random.random((100000, 3)) # random set of points from 0 to 2
+npoints = 100000
+p = 2 * np.random.random((npoints, 3)) # random set of points from 0 to 2
 p -= 1 # from -1 to 1
-p *= 100 # scaled, from -100 to 100
-# translated - centered on (x0, y0, z0)
+p *= max([A, B, C]) # scaled, from -max to +max
+# translated, centered on (x0, y0, z0)
 p[:, 0] += x0
 p[:, 1] += y0
 p[:, 2] += z0
@@ -102,11 +107,26 @@ p2[:, 0] -= x0
 p2[:, 1] -= y0
 p2[:, 2] -= z0
 
-# undo the rotation by taking product of inverse of rotation matrix (which == its transpose) and the untranlated points
+# undo the rotation by taking product of inverse of rotation matrix (which == its transpose) and the untranslated points
 p3 = np.dot(RzRxRy.T, p2.T).T
-i, = np.where((p3[:, 0])**2/A**2 + (p3[:, 1])**2/B**2 + (p3[:, 2])**2/C**2 <= 1) # which points are inside the ellipsoid?
-assert len(i) > 0, "no points fall within ellipsoid"
-pinside = p[i] # pick out those points
 
-# plot the points
-mlab.points3d(pinside[:, 0], pinside[:, 1], pinside[:, 2], mode='point') # plotted points should lie within the ellipse
+# which points are inside the ellipsoid?
+ini, = np.where((p3[:, 0])**2/A**2 + (p3[:, 1])**2/B**2 + (p3[:, 2])**2/C**2 <= 1)
+assert len(ini) > 0, "no points fall within the ellipsoid"
+pin = p[ini] # pick out those points
+
+# which points are outside the ellipsoid?
+alli = np.arange(npoints)
+outi = list(set(alli).difference(ini))
+pout = p[outi]
+
+# plot the points inside
+glyph_in = mlab.points3d(pin[:, 0], pin[:, 1], pin[:, 2], mode='point') # plotted points should lie within the ellipse
+glyph_in.actor.property.color = 1, 1, 1 # inside is white
+
+# plot the points outside
+glyph_out = mlab.points3d(pout[:, 0], pout[:, 1], pout[:, 2], mode='point') # plotted points should lie within the ellipse
+glyph_out.actor.property.color = 0.3, 0.3, 0.3 # outside is grey
+
+# optionally turn off the points outside to better view the points inside
+#glyph_out.visible = False
