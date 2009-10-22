add exporting of textheader, din, and spike times to files for use in neuropy fix bug checking causal order of records, now handle when some are a list of record objects, while others are a struct ndarray update setup.py TODO

spyke/surf.py
@@ -802,6 +802,11 @@ def causalorder(records):
     return True
     '''
     # more straightforward using numpy:
-    ts = np.asarray([ record.TimeStamp for record in records ])
+    try:
+        # won't need this one once all records of each type are stored in
+        # contiguous struct arrays instead of lists of objects
+        ts = np.asarray([ record.TimeStamp for record in records ])
+    except AttributeError:
+        ts = np.asarray([ record['TimeStamp'] for record in records ])
     # is ts in increasing order, ie is difference between subsequent entries >= 0?
     return (np.diff(ts) >= 0).all()