7f19a14230ef5860addfc27f22bf4073b6f5ad15
Martin Spacek <git@mspacek.mm.st>
2010-01-25 14:57:48 -0800

    update pooltest.py to use Py 2.6's built-in multiprocessing module, simplify the example a bit

--- demo/pooltest.py
+++ demo/pooltest.py
@@ -1,17 +1,17 @@
 """Demonstrate use of a processing.Pool"""
 
-from processing import Pool
+from multiprocessing import Pool
 import time
 
-global outputs
-outputs = []
+#global outputs
+#outputs = []
 
 
 def f(x):
-    for i in xrange(10000000): # to slow things down a bit
+    for i in xrange(100000000): # to slow things down a bit
         output = x*x
-    return output
-
+    return [x, output]
+'''
 def handleOutput(output):
     """This f'n, as a callback, is blocking.
     It blocks the whole program, regardless of number of processes or CPUs/cores"""
@@ -20,18 +20,22 @@ def handleOutput(output):
     #print 'pausing'
     #for i in xrange(100000000):
     #    pass
-
+'''
 if __name__ == '__main__':
-    pool = Pool() # create a processing pool with as many processes as there are CPUs/cores on this machine
-    results = []
+    pool = Pool() # create a processing pool with as many processes as there are CPUs/cores
+                  # on this machine, or set arg to n to use exactly n processes
+    #results = []
+    #for i in range(10):
+    #    print 'queueing task %d' % i
+    #    result = pool.apply_async(f, args=(i,), callback=handleOutput) # evaluate f(i) asynchronously
+    #    results.append(result)
+    ncpus = multiprocessing.cpu_count()
     t0 = time.clock()
-    for i in range(10):
-        print 'queueing task %d' % i
-        result = pool.applyAsync(f, args=(i,), callback=handleOutput) # evaluate f(i) asynchronously
-        results.append(result)
-    print 'done queueing tasks, result objects are: %r' % results
+    results = pool.map(f, range(2*ncpus)) # make it int multiple of ncpus for efficiency
+    print('tasks took %.3f sec' % time.clock())
+    #print 'done queueing tasks, result objects are: %r' % results
+    print(results)
     pool.close()
     pool.join()
-    print 'tasks took %.3f sec' % time.clock()
-    print 'outputs: %r' % outputs
-    time.sleep(2) # pause so you can watch the parent process in taskman hang around after worker processes exit
+    #print 'outputs: %r' % outputs
+    time.sleep(10) # pause so you can watch the parent process in taskman hang around after worker processes exit
