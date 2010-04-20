ebea5a497c21bda978ccdca7c307991c772bdbe4
Martin Spacek <git@mspacek.mm.st>
2010-04-20 15:19:31 -0700

    make EM on Gaussian mixture modelling orders of magnitude faster by assuming independence between x and y - whee!

--- demo/gmm.py
+++ demo/gmm.py
@@ -3,6 +3,7 @@
 import numpy as np
 import scipy.io
 import pymix.mixture as pm
+from pylab import figure, scatter
 
 data = scipy.io.loadmat('/data/ptc18/14_full.mat')
 data = data['data']
@@ -12,22 +13,23 @@ mins = data.min(axis=0)
 maxs = data.max(axis=0)
 ranges = maxs - mins
 
-#sigma = np.array([1, 0], [0, 1])
-
-ndistribs = 35
+ndistribs = 100
 distribs = []
-sigma = np.diag(np.ones(nd) * 5000)
 for i in range(ndistribs):
     mu = np.random.random(nd)
     mu = mins + mu * ranges
-    #sigma = np.random.random(nd)
-    #sigma = -1 + 2*sigma # random diagonal values from -1 to 1
-    #sigma = np.diag(sigma) # cov matrix with random diagonal values, and zeros elsewhere
-    distribs.append(pm.MultiNormalDistribution(nd, mu, sigma))
+    xd = pm.NormalDistribution(mu[0], 30)
+    yd = pm.NormalDistribution(mu[1], 30)
+    distrib = pm.ProductDistribution([xd, yd])
+    distribs.append(distrib)
 
 pmdata = pm.DataSet()
 pmdata.fromArray(data)
 
 m = pm.MixtureModel(ndistribs, np.ones(ndistribs)/ndistribs, distribs)
-#m.modelInitialization(pmdata) # this hangs
-m.EM(pmdata, 10, 500)
+#m.modelInitialization(pmdata) # this hangs?
+m.EM(pmdata, 50, 0.1)
+
+clusteris = m.classify(pmdata)
+figure()
+scatter(data[:, 0], data[:, 1], s=1, c=clusteris, edgecolors='none')
