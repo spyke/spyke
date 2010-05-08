1455ddbd88f5aabd0430d2d1228f00857152be2d
Martin Spacek <git@mspacek.mm.st>
2010-05-07 19:09:27 -0700

    implement subsampling of scouts to greatly speed up clustering, while still giving same results - unclustered points are then added to the cluster of the nearest clustered point scale in time according to time duration (1 hour == scaling of 1.0) instead of std of time

--- spyke/climbing.pyx
+++ spyke/climbing.pyx
@@ -4,6 +4,8 @@ cimport cython
 import numpy as np
 cimport numpy as np
 
+import random
+
 cdef extern from "math.h":
     #double sqrt(double x)
     double fabs(double x)
@@ -16,7 +18,8 @@ cdef extern from "stdio.h":
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True) # might be necessary to release the GIL?
-def climb(np.ndarray[np.float32_t, ndim=2] data, double sigma, double alpha):
+def climb(np.ndarray[np.float32_t, ndim=2] data,
+          double sigma, double alpha, int subsample=1, int maxstill=100):
     """Implement Nick's gradient ascent (mountain climbing) clustering algorithm
     TODO:
         - delete scouts that have fewer than n points (at any point during iteration?)
@@ -30,6 +33,7 @@ def climb(np.ndarray[np.float32_t, ndim=2] data, double sigma, double alpha):
             - track the distance each point has travelled during the course of the algorithm. When done, plot the distribution of travel distances, and maybe you'll get something bimodal, and choose a cutoff travel distance past which any point that travelled further is considered a noise point
             - or maybe plot distribution of travel times
             - use some cutoff of local density to specify what's noise and what isn't? skeptical..
+        - NVS - to weed out potential noise spikes, for each cluster, find the local density of the scout point at the max, then reject all other data points in that cluster whose local density falls below, say, 1% of the max. Apply it as a mask, so you can tweak that 1% value as you wish, without having to run the whole algorithm all over again
 
         - visualize algorithm in real time to see what exactly it's doing, and why some clusters are split while others are merged
         - instead of merging the higher indexed scout into the lower indexed one, you should really merge the one with the lower density estimate into the one with the higher density estimate - otherwise you potentially end up deleting the scout that's closer to the local max density, which probably sets you back several iterations
@@ -39,6 +43,7 @@ def climb(np.ndarray[np.float32_t, ndim=2] data, double sigma, double alpha):
 
         - try using simplex algorithm for scout position update step, though that might miss local maxima
         - multithreading/multiprocessing
+            - NVS thinks you could leave the merging step as a thread running in parallel with the gradient step (which itself could be split up easily into multiple threads) - although now with subsampling, the slow steps are mostly the gradient calcs, since there are far fewer scouts to merge to begin with
 
         - add subsampling to reduce initial number of scout points
         - instead of subsampling or duplicating all data as scouts, try starting with a regularly spaced grid of scout points, spaced at intervals slightly less than the smallest cluster. This will especially speed things up for really big data sets, where you'd otherwise start with an enormous number of scouts
@@ -46,6 +51,8 @@ def climb(np.ndarray[np.float32_t, ndim=2] data, double sigma, double alpha):
 
         - rescale all data by 2*sigma so you can get rid of the div by twosigma2 operation?
         - try using the n nearest neighbours to calculate gradient, instead of a guassian with a sigma. This makes it scale free, but NVS says this often results in situations where the gradient is 0 for some reason
+
+        - scale x not just by its std, but also according to some absolute multiple of space (say 1.0 is 50 um), such that recordings with wider or narrower x locations (2 or 3 column probes) will cluster roughly as well with a constant sigma value (like 0.25, which really means you can expect up to 4 clusters along the x axis)
 
     DONE:
         - turn off checks for ZeroDivisionError, though I doubt that slows things down much
@@ -59,11 +66,12 @@ def climb(np.ndarray[np.float32_t, ndim=2] data, double sigma, double alpha):
     """
     cdef int N = len(data) # total num data points
     cdef int ndims = data.shape[1] # num cols in data
-    cdef np.ndarray[np.float32_t, ndim=2] scouts = data.copy() # scouts will be modified
-    cdef int M = N # current num scout points (clusters)
-    cdef np.ndarray[np.int32_t, ndim=1] clusteris = np.arange(N) # cluster indices into data
+    cdef int M # current num scout points (clusters)
+    cdef int nsamples
+    cdef np.ndarray[np.float32_t, ndim=2] scouts
+    cdef np.ndarray[np.int32_t, ndim=1] scoutis # potentially, indices sumsampling data
+    cdef np.ndarray[np.int32_t, ndim=1] clusteris = np.zeros(N, dtype=np.int32) # cluster indices into data
     cdef np.ndarray[np.uint8_t, ndim=1] still = np.zeros(N, dtype=np.uint8) # for each scout, num consecutive iters without significant movement
-    cdef np.uint8_t maxstill = 100
     cdef double sigma2 = sigma**2
     cdef double twosigma2 = 2 * sigma2
     cdef double rmerge = sigma # radius within which scout points are merged
@@ -71,13 +79,27 @@ def climb(np.ndarray[np.float32_t, ndim=2] data, double sigma, double alpha):
     cdef int nneighs # num points in vicinity of scout point
     cdef double rneigh = 4 * sigma # radius around scout to include data for gradient calc
     cdef double rneigh2 = rneigh**2
-    cdef double diff, diff2sum, move, movesum
+    cdef double diff, diff2sum, mindiff2sum, move, movesum
     cdef double minmovesum = 0.0001 * sigma * ndims
     cdef np.ndarray[np.float64_t, ndim=1] diffs = np.zeros(ndims)
     cdef np.ndarray[np.float64_t, ndim=1] diffs2 = np.zeros(ndims)
     cdef np.ndarray[np.float64_t, ndim=1] v = np.zeros(ndims)
     cdef Py_ssize_t i, j, k, scouti, clustii
     cdef int iteri = 0, continuej = 0, merged = 0, nnomerges = 0, maxnnomerges = 1000
+
+    if subsample > 1:
+        # subsample to get a reasonable number of scouts
+        nsamples = N / subsample # this will trunc
+        scoutis = np.asarray(random.sample(xrange(N), nsamples))
+        M = nsamples # initially, but M will decrease over time
+        scouts = data[scoutis].copy() # scouts will be modified
+        clusteris.fill(-1) # -ve number indicates an unclustered data point
+        clusteris[scoutis] = np.arange(M)
+    else:
+        M = N
+        scouts = data.copy() # scouts will be modified
+        clusteris = np.arange(N)
+
 
     while True:
 
@@ -171,6 +193,30 @@ def climb(np.ndarray[np.float32_t, ndim=2] data, double sigma, double alpha):
 
         printf('.')
         iteri += 1
+
+
+    if subsample > 1:
+        # for each unclusterd point, find the closest clustered point, and assign
+        # it to the same cluster
+        print('Finding nearest clustered points for each unclustered point')
+        for j in range(N): # iterate over all data points
+            if clusteris[j] >= 0: # point already has a valid cluster index
+                continue
+            # point is unclustered, find nearest clustered point
+            mindiff2sum = 100e99
+            for i in range(nsamples): # iterate over all clustered points
+                # scoutis is an array of nsamples indices into data that were used as scouts,
+                # and therefore have been clustered
+                scouti = scoutis[i]
+                diff2sum = 0.0 # reset
+                for k in range(ndims):
+                    diff = data[j, k] - data[scouti, k]
+                    diff2sum += diff * diff
+                if diff2sum < mindiff2sum:
+                    # update this unclustered point's cluster index
+                    mindiff2sum = diff2sum
+                    clusteris[j] = clusteris[scouti]
+
 
     nmoving = (still[:M] < maxstill).sum()
     print('\nniters: %d' % iteri)
--- spyke/test_climbing.py
+++ spyke/test_climbing.py
@@ -42,20 +42,31 @@ data = data['data']
 data = np.float32(data)
 '''
 data = np.load('/data/ptc18/tr1/14-tr1-mseq32_40ms_7deg/14_full_x0_y0_t_Vpp.npy')
-data = data[:, :3] # limit npoints and ndims
+data = data[:100000, :3] # limit npoints and ndims
 nd = data.shape[1]
 xstd = data[:, 0].std()
 # normalize x and y by xstd
 for dim in data.T[:2]: # iter over columns:
     dim -= dim.mean()
     dim /= xstd
-# normalize all other dims by their std
-for dim in data.T[2:]: # iter over columns:
-    dim -= dim.mean()
-    dim /= dim.std()
+
+if data.shape[1] > 2:
+    # for time, subtract the min time, divide by the max time (this gives you time from 0 to 1), then scale by 1.0 for every hour of recording
+    t = data.T[2]
+    tmin = t.min()
+    tmax = t.max()
+    tspan = tmax - tmin
+    scale = 1.0 / (60*60*1e6) * tspan
+    t -= tmin
+    t /= tmax
+    t *= scale
+    # normalize all other dims by their std
+    for dim in data.T[3:]: # iter over columns:
+        dim -= dim.mean()
+        dim /= dim.std()
 
 t0 = time.clock()
-clusteris, clusters = climb(data, sigma=0.4, alpha=1.0)
+clusteris, clusters = climb(data, sigma=0.25, alpha=1.0, subsample=10, maxstill=100)
 print('climb took %.3f sec' % (time.clock()-t0))
 
 nclusters = len(clusters)
@@ -70,9 +81,10 @@ f = makefigure()
 scatter(data[:, 0], data[:, 1], s=1, c=samplecolours, edgecolors='none')
 scatter(clusters[:, 0], clusters[:, 1], c=clustercolours)
 
-# plot t vs y
-f = makefigure()
-scatter(data[:, 2], data[:, 1], s=1, c=samplecolours, edgecolors='none')
-scatter(clusters[:, 2], clusters[:, 1], c=clustercolours)
+if data.shape[1] > 2:
+    # plot t vs y
+    f = makefigure()
+    scatter(data[:, 2], data[:, 1], s=1, c=samplecolours, edgecolors='none')
+    scatter(clusters[:, 2], clusters[:, 1], c=clustercolours)
 
 show()
