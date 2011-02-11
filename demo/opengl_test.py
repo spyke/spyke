122392d9d9411e6c618bb3a6a953d6192bcc31b9
Martin Spacek <git@mspacek.mm.st>
2011-02-11 04:08:28 -0800

    demo rotation, translation, and zoom relative to camera coords instead of model coords

--- demo/opengl_test.py
+++ demo/opengl_test.py
@@ -1,5 +1,6 @@
 """PyQt OpenGL example, modified from PySide example, which was a port of the opengl/hellogl
 example from Qt v4.x"""
+
 from __future__ import division
 
 import sys
@@ -17,9 +18,10 @@ YELLOW = 255, 255, 0
 MAGENTA = 255, 0, 255
 CMAP = np.array([RED, GREEN, BLUE, YELLOW, MAGENTA], dtype=np.uint8)
 
+'''
 def normdeg(angle):
     return angle % 360
-
+'''
 
 class Window(QtGui.QWidget):
     def __init__(self, parent=None):
@@ -37,15 +39,6 @@ class Window(QtGui.QWidget):
 class GLWidget(QtOpenGL.QGLWidget):
     def __init__(self, parent=None):
         QtOpenGL.QGLWidget.__init__(self, parent)
-
-        #self.object = 0
-        self.x = 0.0
-        self.y = 0.0
-        self.z = -2.0 # 0 would put us directly inside the random cube of points
-        self.xrot = 0
-        self.yrot = 0
-        self.zrot = 0
-
         self.lastPos = QtCore.QPoint()
         '''
         format = QtOpenGL.QGLFormat()
@@ -71,113 +64,105 @@ class GLWidget(QtOpenGL.QGLWidget):
         return QtCore.QSize(400, 400)
 
     def initializeGL(self):
-        # these are the defaults anyway, but just to be thorough
-        GL.glClearColor(0.0, 0.0, 0.0, 1.0)
-        GL.glClearDepth(1.0)
-
-        #GL.glEnable(GL.GL_POINT_SMOOTH) # doesn't seem to work right, proper way to antialiase?
-        #GL.glEnable(GL.GL_LINE_SMOOTH) # works better
-        #GL.glPointSize(1.5) # truncs to the nearest pixel if antialiasing is off
-        # float32 is much faster than float64
-        self.npoints = 7000000
+        self.npoints = 100000
         self.points = np.float32(np.random.random((self.npoints, 3))) - 0.5
         self.cis = np.uint8(np.random.randint(5, size=self.npoints))
         self.colors = CMAP[self.cis] # uint8
-        #n = int(self.npoints / 5)
-        #self.n = n
-        #self.i0 = np.arange(n, dtype=np.int32)
-        #self.i1 = np.arange(n, 2*n, dtype=np.int32)
-        #self.i2 = np.arange(2*n, 3*n, dtype=np.int32)
-        #self.i3 = np.arange(3*n, 4*n, dtype=np.int32)
-        #self.i4 = np.arange(4*n, 5*n, dtype=np.int32)
 
-
+        GL.glClearColor(0.0, 0.0, 0.0, 1.0) # same as default
+        GL.glClearDepth(1.0) # same as default
         GL.glEnable(GL.GL_DEPTH_TEST) # display points according to occlusion, not order of plotting
-
-        #self.object = self.makeObject()
+        #GL.glEnable(GL.GL_POINT_SMOOTH) # doesn't seem to work right, proper way to antialiase?
+        #GL.glEnable(GL.GL_LINE_SMOOTH) # works better
+        #GL.glPointSize(1.5) # truncs to the nearest pixel if antialiasing is off
         #GL.glShadeModel(GL.GL_FLAT)
         #GL.glEnable(GL.GL_CULL_FACE) # only useful for solids
+        GL.glTranslate(0, 0, -3) # initial distance from origin
 
     def paintGL(self):
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
 
-        # TODO: try storing vertex array in buffer object on server side (graphics memory?)
-        # for faster rendering?
+        # Don't load identity matrix. Do all transforms in place against current matrix
+        # and take advantage of OpenGL's state-machineness.
+        # Sure, you might get round-off error over time, but who cares? If you wanna return
+        # to a specific focal point on 'f', that's when you really need to first load the
+        # identity matrix before doing the transforms
+        #GL.glLoadIdentity() # loads identity matrix into top of matrix stack
 
-        # TODO: make rotation relative to eye coords, not modelview coords
+        #GLU.gluLookAt() # viewing transform for camera: where placed, where pointed, which way is up
+        #GL.glScale() # modelling transformation, lets you stretch your objects
 
-        # this rotation and translation stuff seems wasteful here.
-        # can't we just do this only on mouse/keyboard input, update the modelview
-        # matrix, and then leave it as is in self.paintGL?
-        GL.glLoadIdentity() # loads identity matrix into top of matrix stack
-        GL.glTranslate(self.x, self.y, self.z) # zval zooms you in and out
-        GL.glRotate(self.xrot, 1.0, 0.0, 0.0) # angles in deg
-        GL.glRotate(self.yrot, 0.0, 1.0, 0.0)
-        GL.glRotate(self.zrot, 0.0, 0.0, 1.0)
-
-        #GL.glCallList(self.object)
         GL.glEnableClientState(GL.GL_COLOR_ARRAY);
         GL.glEnableClientState(GL.GL_VERTEX_ARRAY);
-        #GL.glEnableClientState(GL.GL_INDEX_ARRAY);
         GL.glColorPointerub(self.colors) # usigned byte, ie uint8
         GL.glVertexPointerf(self.points) # float32
-        #GL.glIndexPointeri(self.cis) # int32
 
-        #GL.glColor(*RED)
         GL.glDrawArrays(GL.GL_POINTS, 0, self.npoints)
         # might consider using buffer objects for even more speed (less unnecessary vertex
         # data from ram to vram, I think). Apparently, buffer objects don't work with
         # color arrays?
 
-        #GL.glColor(*RED)
-        #GL.glDrawArrays(GL.GL_POINTS, 0, self.npoints) # faster than glDrawElements?
-
-        '''
-        # glDrawArrays is a lot faster than glDrawElements:
-        n = self.n
-        GL.glColor(*RED)
-        GL.glDrawArrays(GL.GL_POINTS, 0, n)
-        GL.glColor(*GREEN)
-        GL.glDrawArrays(GL.GL_POINTS, n, n)
-        GL.glColor(*BLUE)
-        GL.glDrawArrays(GL.GL_POINTS, 2*n, n)
-        GL.glColor(*YELLOW)
-        GL.glDrawArrays(GL.GL_POINTS, 3*n, n)
-        GL.glColor(*MAGENTA)
-        GL.glDrawArrays(GL.GL_POINTS, 4*n, n)
-        '''
-        '''
-        GL.glColor(*RED)
-        GL.glDrawElementsui(GL.GL_POINTS, self.i0) # treat array indices as unsigned int
-        GL.glColor(*GREEN)
-        GL.glDrawElementsui(GL.GL_POINTS, self.i1)
-        GL.glColor(*BLUE)
-        GL.glDrawElementsui(GL.GL_POINTS, self.i2)
-        GL.glColor(*YELLOW)
-        GL.glDrawElementsui(GL.GL_POINTS, self.i3)
-        GL.glColor(*MAGENTA)
-        GL.glDrawElementsui(GL.GL_POINTS, self.i4)
-        '''
         #GL.glFlush() # forces drawing to begin, only makes difference for client-server?
         #self.swapBuffers()
 
+        # print the modelview matrix
+        MV = GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX)
+        print(MV)
+
     def resizeGL(self, width, height):
-        #side = max(width, height)
-        #GL.glViewport((width-side)/2, (height-side)/2, side, side)
         GL.glViewport(0, 0, width, height)
-        # specify clipping box for perspective projection
         GL.glMatrixMode(GL.GL_PROJECTION)
         GL.glLoadIdentity()
-        #GL.glFrustum(-5, 5, -5, 5, 5, 20.) # nearz (2nd last arg) also zooms you in and out?
         # fov (deg) controls amount of perspective, and as a side effect initial apparent size
         GLU.gluPerspective(45, width/height, 0.0001, 1000) # fov, aspect, nearz & farz clip planes
         GL.glMatrixMode(GL.GL_MODELVIEW)
 
-        # specify clipping box for orthonormal projection
-        #GL.glMatrixMode(GL.GL_PROJECTION)
-        #GL.glLoadIdentity()
-        #GL.glOrtho(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0)
-        #GL.glMatrixMode(GL.GL_MODELVIEW)
+    # modelview matrix is column major, so we work on columns instead of rows
+    def getViewRight(self):
+        """View right vector: 1st col of modelview matrix"""
+        return GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX)[:, 0]
+
+    def getViewUp(self):
+        """View up vector: 2nd col of modelview matrix"""
+        return GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX)[:, 1]
+
+    def getViewNormal(self):
+        """View normal vector: 3rd col of modelview matrix"""
+        return GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX)[:, 2]
+
+    def getTranslation(self):
+        """Translation vector: 4th row of modelview matrix"""
+        return GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX)[3]
+
+    def pan(self, dx, dy):
+        """Translate along view right and view up vectors"""
+        vr = self.getViewRight()
+        vr *= dx
+        GL.glTranslate(vr[0], vr[1], vr[2])
+        vu = self.getViewUp()
+        vu *= dy
+        GL.glTranslate(vu[0], vu[1], vu[2])
+
+    def zoom(self, dr):
+        """Translate along view normal vector"""
+        vn = self.getViewNormal()
+        vn *= dr
+        GL.glTranslate(vn[0], vn[1], vn[2])
+
+    def pitch(self, dangle): # aka elevation
+        """Rotate around view right vector"""
+        vr = self.getViewRight()
+        GL.glRotate(dangle, vr[0], vr[1], vr[2])
+
+    def yaw(self, dangle): # aka azimuth
+        """Rotate around view up vector"""
+        vu = self.getViewUp()
+        GL.glRotate(dangle, vu[0], vu[1], vu[2])
+
+    def roll(self, dangle):
+        """Rotate around view normal vector"""
+        vn = self.getViewNormal()
+        GL.glRotate(dangle, vn[0], vn[1], vn[2])
 
     def mousePressEvent(self, event):
         self.lastPos = QtCore.QPoint(event.pos())
@@ -189,58 +174,62 @@ class GLWidget(QtOpenGL.QGLWidget):
         dy = event.y() - self.lastPos.y()
 
         if buttons == QtCore.Qt.LeftButton:
-            if modifiers == Qt.ControlModifier: # rotate around z
-                self.zrot = normdeg(self.zrot - 0.5*dx - 0.5*dy) # rotates around the model's z axis, but really what we want is to rotate the scene around the viewer's normal axis
-            elif modifiers == Qt.ShiftModifier: # translate in x and y
-                self.x += dx / 100
-                self.y -= dy / 100
-            else: # rotate around x and y
-                self.xrot = normdeg(self.xrot + 0.5*dy)
-                self.yrot = normdeg(self.yrot + 0.5*dx)
-        elif buttons == QtCore.Qt.RightButton: # zoom
-            self.z -= dy / 40
+            if modifiers == Qt.ControlModifier:
+                self.roll(-0.5*dx - 0.5*dy)
+            elif modifiers == Qt.ShiftModifier:
+                self.pan(dx/100, -dy/100) # qt viewport y axis points down
+            else:
+                self.yaw(0.5*dx)
+                self.pitch(0.5*dy)
+        elif buttons == QtCore.Qt.RightButton:
+            self.zoom(-dy/100) # qt viewport y axis points down
 
         self.updateGL()
         self.lastPos = QtCore.QPoint(event.pos())
 
     def wheelEvent(self, event):
-        self.z += event.delta() / 500 # zoom
+        self.zoom(event.delta() / 500)
         self.updateGL()
 
     def keyPressEvent(self, event):
         key = event.key()
         modifiers = event.modifiers()
 
-        if modifiers == Qt.ControlModifier: # rotate around z, or zoom along z
+        if modifiers == Qt.ControlModifier:
             if key == Qt.Key_Left:
-                self.zrot = normdeg(self.zrot + 5)
+                self.roll(5)
             elif key == Qt.Key_Right:
-                self.zrot = normdeg(self.zrot - 5)
+                self.roll(-5)
             elif key == Qt.Key_Up:
-                self.z += 0.2
+                self.zoom(0.2)
             elif key == Qt.Key_Down:
-                self.z -= 0.2
-        elif modifiers == Qt.ShiftModifier: # translate in x and y
+                self.zoom(-0.2)
+        elif modifiers == Qt.ShiftModifier:
             if key == Qt.Key_Left:
-                self.x -= 0.2
+                self.pan(-0.2, 0)
             elif key == Qt.Key_Right:
-                self.x += 0.2
+                self.pan(0.2, 0)
             elif key == Qt.Key_Up:
-                self.y += 0.2
+                self.pan(0, 0.2)
             elif key == Qt.Key_Down:
-                self.y -= 0.2
-        else: # rotate around x and y
+                self.pan(0, -0.2)
+        else:
             if key == Qt.Key_Left:
-                self.yrot = normdeg(self.yrot - 5)
+                self.yaw(-5)
             elif key == Qt.Key_Right:
-                self.yrot = normdeg(self.yrot + 5)
+                self.yaw(5)
             elif key == Qt.Key_Up:
-                self.xrot = normdeg(self.xrot - 5)
+                self.pitch(-5)
             elif key == Qt.Key_Down:
-                self.xrot = normdeg(self.xrot + 5)
-
-        if key == Qt.Key_0: # reset focus
-            self.x, self.y = 0.0, 0.0
+                self.pitch(5)
+            elif key == Qt.Key_0: # reset view to origin, maintaining distance
+                # this works, but has some roundoff error:
+                #vt = self.getTranslation()
+                #self.pan(-vt[0], -vt[1])
+                # this is equivalent, but with no roundoff error:
+                MV = GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX)
+                MV[3, 0:2] = 0 # set first two entries of 4th row to 0
+                GL.glLoadMatrixd(MV)
 
         self.updateGL()
 
