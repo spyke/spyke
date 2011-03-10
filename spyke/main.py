normalize gradient by kernel instead of nneighs - more correct, and might be faster too replace some temporary 1D numpy arrays with pure C arrays for speed print 'M' for merge no more than once per iteration remove unused code related to calculating scout and point densities fix widget tab order in cluster pane

spyke/main.py
@@ -556,11 +556,10 @@ class SpykeWindow(QtGui.QMainWindow):
             t0 = time.time()
             results = climb(data, sigma=s.sigmasqrtndims, alpha=s.alpha, rmergex=s.rmergex,
                             rneighx=s.rneighx, nsamples=s.nsamples,
-                            calcpointdensities=False, calcscoutdensities=False,
                             clusterunsampledpoints=s.clusterunsampledspikes,
                             minmove=-1.0, maxstill=s.maxstill, maxnnomerges=1000,
                             minpoints=s.minpoints)
-            cids, scoutpositions, densities, scoutdensities, sampleis = results
+            cids, scoutpositions, sampleis = results
             nids = list(np.unique(cids))
             try: nids.remove(-1)
             except ValueError: pass