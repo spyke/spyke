ac89b31cbf68895ea85de5cf90f23f3ea2730e63
Martin Spacek <git@mspacek.mm.st>
2011-06-24 15:30:20 -0700

    realized can't declare ndarrays with arbitrary ndims at compile time, have to keep big sparse arrays as linear C arrays defined pure C prod() and ndi2li() comment cleanup

--- spyke/climbing.pyx
+++ spyke/climbing.pyx
@@ -17,7 +17,7 @@ cdef extern from "math.h":
 cdef extern from "stdio.h":
     int printf(char *, ...) nogil
     cdef void *malloc(size_t) nogil # allocates without clearing to 0
-    #cdef void *calloc(size_t, size_t) nogil # allocates with clearing to 0
+    cdef void *calloc(size_t, size_t) nogil # allocates with clearing to 0
     cdef void free(void *) nogil
 
 cdef extern from "string.h":
@@ -44,34 +44,59 @@ def climb(np.ndarray[np.float32_t, ndim=2, mode='c'] data,
         - maybe annealing of alpha (decreasing it over time)? NVS sounds skeptical
 
         - classify obvious wide flat areas as noise points that shouldn't be clustered:
-            - track the distance each point has travelled during the course of the algorithm. When done, plot the distribution of travel distances, and maybe you'll get something bimodal, and choose a cutoff travel distance past which any point that travelled further is considered a noise point
+            - track the distance each point has travelled during the course of the algorithm.
+            When done, plot the distribution of travel distances, and maybe you'll get something
+            bimodal, and choose a cutoff travel distance past which any point that travelled
+            further is considered a noise point
             - or maybe plot distribution of travel times
             - use some cutoff of local density to specify what's noise and what isn't? skeptical..
 
-        - visualize algorithm in real time to see what exactly it's doing, and why some clusters are split while others are merged
+        - visualize algorithm in real time to see what exactly it's doing, and why some clusters
+        are split while others are merged
 
-        - instead of merging the higher indexed scout into the lower indexed one, you should really merge the one with the lower density estimate into the one with the higher density estimate - otherwise you potentially end up deleting the scout that's closer to the local max density, which probably sets you back several iterations
-            - this would require calc'ing and storing the density for each cluster, and updating it every time it moves
-                - is local density and local gradient calc sufficiently similar that this won't be expensive?
-            - find whichever has the biggest density estimate - if it's not the lowest indexed scout (which will be the case 50% of the time), swap the entries in all the arrays (scouts, still, etc) except for the cids array, then proceed as usual. Then update the density for the newly merged cluster
+        - instead of merging the higher indexed scout into the lower indexed one, you should
+        really merge the one with the lower density estimate into the one with the higher
+        density estimate - otherwise you potentially end up deleting the scout that's closer
+        to the local max density, which probably sets you back several iterations
+            - this would require calc'ing and storing the density for each cluster, and updating
+            it every time it moves
+                - is local density and local gradient calc sufficiently similar that this won't
+                be expensive?
+            - find whichever has the biggest density estimate - if it's not the lowest indexed
+            scout (which will be the case 50% of the time), swap the entries in all the arrays
+            (scouts, still, etc) except for the cids array, then proceed as usual. Then update
+            the density for the newly merged cluster
 
-        - try using simplex algorithm for scout position update step, though that might miss local maxima
+        - try using simplex algorithm for scout position update step, though that might miss
+        local maxima
 
-        - rescale all data by 2*sigma so you can get rid of the div by twosigma2 operation? - only applies to Gaussian kernel, not Cauchy
+        - rescale all data by 2*sigma so you can get rid of the div by twosigma2 operation?
+            - only applies to Gaussian kernel, not Cauchy
 
-        - try using the n nearest neighbours to calculate gradient, instead of a guassian with a sigma. This makes it scale free, but NVS says this often results in situations where the gradient is 0 for some reason
+        - try using the n nearest neighbours to calculate gradient, instead of a guassian with
+        a sigma. This makes it scale free, but NVS says this often results in situations where
+        the gradient is 0 for some reason
 
-        - scale x not just by its std, but also according to some absolute multiple of space (say 1.0 is 50 um), such that recordings with wider or narrower x locations (2 or 3 column probes) will cluster roughly as well with a constant sigma value (like 0.25, which really means you can expect up to 4 clusters along the x axis)
+        - scale x not just by its std, but also according to some absolute multiple of space
+        (say 1.0 is 50 um), such that recordings with wider or narrower x locations (2 or 3
+        column probes) will cluster roughly as well with a constant sigma value (like 0.25,
+        which really means you can expect up to 4 clusters along the x axis)
 
     DONE:
         - turn off checks for ZeroDivisionError, though I doubt that slows things down much
         - keep track of max movement on each iter, use consistently low max movement as
           automatic exit criteria
-            - alternative: keep track of how long it's been since the last scout merger, and exit based on that
+            - alternative: keep track of how long it's been since the last scout merger, and
+            exit based on that
         - add freezing of points, for speed?
-            - when a scout point has moved less than some distance per iteration for all of the last n iterations, freeze it. Then, in the position update loop, check for frozen scout points
+            - when a scout point has moved less than some distance per iteration for all of
+            the last n iterations, freeze it. Then, in the position update loop, check for
+            frozen scout points
         - add subsampling to reduce initial number of scout points
-        - NVS - to weed out potential noise spikes, for each cluster, find the local density of the scout point at the max, then reject all other data points in that cluster whose local density falls below, say, 1% of the max. Apply it as a mask, so you can tweak that 1% value as you wish, without having to run the whole algorithm all over again
+        - NVS - to weed out potential noise spikes, for each cluster, find the local density
+        of the scout point at the max, then reject all other data points in that cluster whose
+        ocal density falls below, say, 1% of the max. Apply it as a mask, so you can tweak that
+        1% value as you wish, without having to run the whole algorithm all over again
         - delete scouts that have fewer than n points (at any point during iteration?)
         - multithread scout update and assignment of unclustered points step
     """
@@ -80,16 +105,13 @@ def climb(np.ndarray[np.float32_t, ndim=2, mode='c'] data,
     cdef int M # current num scout points (clusters)
     cdef int npoints, npointsremoved, nclustsremoved
     
-
     # scouts table starts out as just a copy of float data table
     cdef np.ndarray[np.float32_t, ndim=2, mode='c'] scouts = data.copy() # stores scout positions
-
     
     # need to first convert data table to something suitable for truncing to easily get
     # ints. First have to add some offset to make everything +ve. Then, divide by your fraction
     # of sigma that you want to discretize by. Then, when returning scout positions, need to do 
     # the inverse
-    
     mindata = min(data)
     data -= mindata # offset data to be +ve starting from 0
     binx = 0.1 # some fraction of sigma to bin data by
@@ -97,32 +119,35 @@ def climb(np.ndarray[np.float32_t, ndim=2, mode='c'] data,
     data /= binsize # scale data
     assert data.max() < 2**32
     assert data.min() >= 0
-    data = np.uint32(data) # trunc to int
+    data = np.uint32(data) # trunc to uint32
 
     # get dimensions of sparse matrices
     cdef np.ndarray[np.uint32_t, ndim=1, mode='c'] dims = np.zeros(ndims, dtype=np.uint32)
-    for i in range(ndims):
-        dims[i] = max(data[:, i])
+    for dimi in range(ndims):
+        dims[dimi] = max(data[:, dimi])
     
     # ndim static histogram of point positions in data, bins of size binsize
     # use uint16, since not likely to have more than 65k points in a single bin.
     # Hell, maybe uint8 would work too
-    cdef np.ndarray[np.uint16_t, ndim=ndims, mode='c'] datah = np.zeros(dims, dtype=np.uint16)
-    
-    # this is gonna be super slow for now, cythonize later:
-    for point in data:
-        # need to convert point to tuple to interpret it as index of single point in datah
-        index = tuple(point)
-        datah[index] += 1
-        if datah[index] = 2**16 - 1:
+    #cdef np.ndarray[np.uint16_t, ndim=ndims, mode='c'] datah = np.zeros(dims, dtype=np.uint16)
+    datah = <unsigned short *>calloc(prod(dims)*sizeof(unsigned short))
+    for i in range(N):
+        j = ndi2li(data[pi]) # convert ndim index to linear index
+        datah[j] += 1
+        if datah[j] == 2**16 - 1:
             raise RuntimeError("uint16 isn't enough for datah!")
     
     ## unravel_index and ravel_multi_index are useful!
     
     # ndim dynamic histogram of scout positions in scouts table
-    # use dynamic scoutspace sparse matrix to approximate each scout's position and calculate gradient according to same sized datah, but then update the scout's actual position in separate scouts table in float, per usual. Otherwise, if you stored scout positions quantized, you could easily get stuck in a bin and never get out, because you could never accumulate less than bin sized changes in position
-    cdef np.ndarray[np.uint32_t, ndim=ndims, mode='c'] scoutspace = np.zeros(dims, dtype=np.uint32)
-
+    # use dynamic scoutspace sparse matrix to approximate each scout's position and
+    # calculate gradient according to same sized datah, but then update the scout's
+    # actual position in separate scouts table in float, per usual. Otherwise, if you
+    # stored scout positions quantized, you could easily get stuck in a bin and never
+    # get out, because you could never accumulate less than bin sized changes in position
+    print('creating %d MB scoutspace matrix' % (dims.prod() * 4 / 1e6))
+    #cdef np.ndarray[np.uint32_t, ndim=ndims, mode='c'] scoutspace = np.zeros(dims, dtype=np.uint32)
+    scoutspace = <unsigned int *>calloc(prod(dims)*sizeof(unsigned int))
 
     # for merging scouts, clear scoutspace, and start writing their indices to it.
     # While writing, if you find the position in the matrix is already occupied,
@@ -135,7 +160,7 @@ def climb(np.ndarray[np.float32_t, ndim=2, mode='c'] data,
     # cluster indices into data:
     cdef np.ndarray[np.int32_t, ndim=1, mode='c'] cids = np.zeros(N, dtype=np.int32)
     # for each scout, num consecutive iters without significant movement:
-    ## TODO: should check that maxstill < 256, or use uint16 instead:
+    ## TODO: should check that (maxstill < 256).all(), or use uint16 instead:
     cdef np.ndarray[np.uint8_t, ndim=1, mode='c'] still = np.zeros(N, dtype=np.uint8)
     cdef double sigma2 = sigma * sigma
     #cdef double twosigma2 = 2 * sigma2
@@ -370,3 +395,38 @@ cdef void span(long *lohi, int start, int end, int N) nogil:
     for i in range(N):
         lohi[i] = start + step*i
     lohi[N] = end
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef long prod(np.ndarray[np.uint32_t, ndim=1, mode='c'] a) nogil:
+    """Return product of entries in uint32 array a"""
+    cdef long result, n
+    cdef Py_ssize_t i
+    n = a.shape[0] # this doesn't invoke Python apparently
+    result = 1
+    for i in range(n):
+        result *= a[i]
+    return result
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef long long ndi2li(np.ndarray[np.uint32_t, ndim=1, mode='c'] ndi,
+                      np.ndarray[np.uint32_t, ndim=1, mode='c'] dims) nogil:
+    """Convert n dimensional index in array ndi to linear index. ndi
+    and dims should be the same length, and each entry in ndi should be
+    less than its corresponding dimension size in dims"""
+    cdef long long li, pr=1
+    cdef Py_ssize_t di, ndims
+    ndims = ndi.shape[0]
+    li = ndi[ndims-1] # init with index of deepest dimension
+    # iterate from ndims-1 to 0, from 2nd deepest to shallowest dimensions, either
+    # syntax works, and both seem to be C optimized:
+    #for di in range(ndims-1, 0, -1):
+    for di from ndims-1 >= di > 0:
+        printf('di=%d\n', di)
+        pr *= dims[di] # running product of dimensions
+        li += ndi[di-1] * pr # accum sum of products of current ndi and all deeper dimensions
+    return li
+    
