56675c322d6470efc53b950076020bafe5671550
Martin Spacek <git@mspacek.mm.st>
2012-10-10 12:05:47 -0700

    make minus key (designate multiunit cluster) work from within cluster window

--- spyke/cluster.py
+++ spyke/cluster.py
@@ -749,8 +749,8 @@ class GLWidget(QtOpenGL.QGLWidget):
             sw.spykewindow.ui.plotButton.click() # same as hitting ENTER in nslist
         elif key == Qt.Key_F11:
             self.parent().keyPressEvent(event) # pass it on to parent Cluster window
-        elif key in [Qt.Key_Escape, Qt.Key_Delete, Qt.Key_M, Qt.Key_G, Qt.Key_Slash,
-                     Qt.Key_Backslash, Qt.Key_NumberSign, Qt.Key_R,
+        elif key in [Qt.Key_Escape, Qt.Key_Delete, Qt.Key_M, Qt.Key_G, Qt.Key_Minus,
+                     Qt.Key_Slash, Qt.Key_Backslash, Qt.Key_NumberSign, Qt.Key_R,
                      Qt.Key_Space, Qt.Key_B, Qt.Key_Comma, Qt.Key_Period]:
             sw.keyPressEvent(event) # pass it on to Sort window
 
