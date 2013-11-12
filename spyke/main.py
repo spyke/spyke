c650c46b508fadc847a358a930f2d17759ec8549
Martin Spacek <git@mspacek.mm.st>
2013-11-11 20:42:07 -0800

    Change notation in compareAllAutoCorrPairs, comments

--- spyke/main.py
+++ spyke/main.py
@@ -1439,24 +1439,28 @@ class SpykeWindow(QtGui.QMainWindow):
 
     @QtCore.pyqtSlot()
     def on_compareAllAutoCorrPairs_clicked(self, RP=750):
-        """For each neuron pair, calculate autocorr of each neuron, find number of ISIs <=
-        RP, sum this up for both neurons, then temporarily merge the spike trains of the two
-        neurons, calculate autocorr of merger, and find its number of ISIs <= RP. Take
-        difference between this and the previous sum, normalize by total number of intervals
-        to get change in refractory period contamination drpc. Sort neuron pairs in
-        decreasing order of drpc. Those with the biggest drpc have an autocorr that's most
-        affected by the incorrect merger. This should help demonstrate that even in the
-        worst case scenario, the autocorr is a poor diagnostic of contaminated spike
-        trains."""
+        """For each neuron pair, calculate autocorr of each neuron, find number of spike
+        intervals <= RP, sum this up for both neurons, then temporarily merge the spike trains
+        of the two neurons. Calculate autocorr of merger, and find its number of spike
+        intervals <= RP. Take difference between this and the previous sum, normalize by total
+        number of ISIs to get change in fraction of refractory period violations df. Sort
+        neuron pairs in decreasing order of df. Those with the biggest df have an autocorr
+        that's most affected by the incorrect merger. This should help demonstrate that even
+        in the worst case scenario, the autocorr is a poor diagnostic of contaminated spike
+        trains. Not that in the numerator of the df calcalation, I should really be counting
+        number of consecutive ISIs <= RP, not number of spike intervals in general in the
+        autocorr <= RP, but the two are very nearly the same thing. If there's one spike in
+        that interval, it's very unlikely for there to be another, and so ISI and autocorr for
+        such a short interval are effectively the same thing."""
         sort = self.sort
         nids = sort.good
         nn = len(nids)
         npairs = comb(nn, 2, exact=True)
         print('RP = %d' % RP)
         print('npairs = %d' % npairs)
-        # refractor period contamination array, one row per neuron pair:
-        # each row: [nid0, nid1, rpc0, rpc1, drpc]
-        rpc = np.zeros((npairs, 5), dtype=np.float64)
+        # refractory period violation array, one row per neuron pair:
+        # each row: [nid0, nid1, f0, f1, df]
+        f = np.zeros((npairs, 5), dtype=np.float64)
         # convert RP to half trange array, in us
         trange = np.array([0, RP])
         pairi = 0
@@ -1468,6 +1472,11 @@ class SpykeWindow(QtGui.QMainWindow):
                 sids1 = sort.neurons[nid1].sids
                 s0 = sort.spikes['t'][sids0]
                 s1 = sort.spikes['t'][sids1]
+                # taking xcorr here really isn't necessary, only really need to take
+                # diff of each spike train and count the number of consecutive intervals
+                # (ISIs) that fall below RP, but this is the way I happened to do it at the
+                # time, and I don't want to regenerate my plots at the moment, so I'm leaving
+                # it be for now:
                 dts0 = util.xcorr(s0, s0, trange)
                 dts0 = dts0[dts0 != 0] # remove 0s for autocorr
                 dts1 = util.xcorr(s1, s1, trange)
@@ -1480,24 +1489,24 @@ class SpykeWindow(QtGui.QMainWindow):
                 nISI0 = len(s0) - 1
                 nISI1 = len(s1) - 1
                 nISI = len(s) - 1
-                rpc0 = len(dts0) / nISI0
-                rpc1 = len(dts1) / nISI1
-                drpc = (len(dts) - (len(dts0) + len(dts1))) / nISI
-                rpc[pairi] = nid0, nid1, rpc0, rpc1, drpc
+                f0 = len(dts0) / nISI0
+                f1 = len(dts1) / nISI1
+                df = (len(dts) - (len(dts0) + len(dts1))) / nISI
+                f[pairi] = nid0, nid1, f0, f1, df
                 pairi += 1
                 print('.', end='')
-        sortis = np.argsort(rpc[:, 4])
+        sortis = np.argsort(f[:, 4])
         sortis = sortis[::-1] # reverse
-        rpc = rpc[sortis] # sorted by decreasing drpc values
-        print(rpc)
+        f = f[sortis] # sorted by decreasing df values
+        print(f)
         print('first <= 50 pairs:')
-        print(rpc[:50])
+        print(f[:50])
         f = pl.figure()
-        pl.get_current_fig_manager().set_window_title('delta RPC')
-        pl.plot(rpc[:, 4], 'k-', linewidth=4)
+        pl.get_current_fig_manager().set_window_title('delta f')
+        pl.plot(f[:, 4], 'k-', linewidth=4)
         pl.xlabel('pair number')
-        #pl.ylabel(r'$\Delta$RPC for ISI $\leq$ %.2f ms' % (RP/1000))
-        pl.ylabel(r'$\Delta$RPC')
+        #pl.ylabel(r'$\Delta$f for ISI $\leq$ %.2f ms' % (RP/1000))
+        pl.ylabel(r'$\Delta$f')
         import pdb; pdb.set_trace()
 
     @QtCore.pyqtSlot()
