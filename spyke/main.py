5d9e4e7294586e4510b87311c94ba98b2ca45eaf
Martin Spacek <git@mspacek.mm.st>
2014-09-22 13:40:07 -0700

    Update About box links

--- spyke/main.py
+++ spyke/main.py
@@ -639,10 +639,11 @@ class SpykeWindow(QtGui.QMainWindow):
                                  QtCore.QT_VERSION_STR, QtCore.PYQT_VERSION_STR,
                                  platform.platform())
         text = """
-        <h2><a href=http://swindale.ecc.ubc.ca/spyke>spyke</a> %s</h2>
+        <h2><a href=http://spyke.github.io>spyke</a> %s</h2>
         <p>A tool for neuronal waveform visualization and spike sorting</p>
 
-        <p>Copyright &copy; 2008-2014 Martin Spacek, Reza Lotun<br>
+        <p>Copyright &copy; 2008-2014 <a href=http://mspacek.github.io>Martin Spacek</a>,
+                                                                       Reza Lotun<br>
            <a href=http://swindale.ecc.ubc.ca>Swindale</a> Lab,
            University of British Columbia</p>
 
