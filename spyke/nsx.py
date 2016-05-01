81658aa5a96f600d9cb7d31f081ea900cfcc4fb1
Martin Spacek <git@mspacek.mm.st>
2016-05-01 13:30:42 +0200

    Complete .nsx file header parsing

--- spyke/nsx.py
+++ spyke/nsx.py
@@ -1,4 +1,9 @@
-"""Load Blackrock Neural Signal Processing System-generated .nsx files."""
+"""Load Blackrock Neural Signal Processing System .nsx files.
+
+Based on file documentation at:
+
+http://support.blackrockmicro.com/KB/View/166838-file-specifications-packet-details-headers-etc
+"""
 
 from __future__ import division
 
@@ -69,23 +74,51 @@ class FileHeader(object):
     """.nsx file header. Takes an open file, parses in from current file
     pointer position, stores header fields as attribs"""
 
-    #def __len__(self):
-    #    return self.nbytes
+    def __len__(self):
+        return self.nbytes
 
     def parse(self, f):
-        # basic header:
+        # "basic" header:
         self.offset = f.tell()
         self.filetype = f.read(8)
         assert self.filetype == 'NEURALCD'
-        self.version = unpack('BB', f.read(2))# aka "File Spec", major and minor versions
-        self.nbytes, = unpack('I', f.read(4))
+        self.version = unpack('BB', f.read(2)) # aka "File Spec", major and minor versions
+        self.nbytes, = unpack('I', f.read(4)) # length of full header, in bytes
         self.label = f.read(16).rstrip(NULL) # sampling group label, null terminated
-        self.comment = f.read(256).rstrip(NULL).lstrip(NULL)
-        self.decimation, = unpack('I', f.read(4)) # aka "Period", wrt 30 kHz sampling rate
-        self.sampfreq, = unpack('I', f.read(4)) # Hz
+        self.comment = f.read(256).rstrip(NULL)
+        # "Period", wrt 30 kHz sampling freq; sampling freq in Hz:
+        self.decimation, self.sampfreq = unpack('II', f.read(8))
         # date and time corresponding to t=0
         year, month, dow, day, hour, m, s, ms = unpack('HHHHHHHH', f.read(16))
         self.datetime = datetime.datetime(year, month, day, hour, m, s, ms)
         self.nchans, = unpack('I', f.read(4))
 
-        # extended header:
+        # "extended" headers, each one describing a channel
+        self.chans = {}
+        for chani in range(self.nchans):
+            chanheader = ChanHeader()
+            chanheader.parse(f)
+            self.chans[chanheader.id] = chanheader
+        assert len(self.chans) == self.nchans # make sure each chan ID is unique
+        assert len(self) == f.tell() # header should be of expected length
+
+
+class ChanHeader(object):
+    """.nsx header information for a single channel"""
+
+    def parse(self, f):
+        self.type = f.read(2)
+        assert self.type == 'CC' # for "continuous channel"
+        self.id, = unpack('H', f.read(2)) # aka "electrode ID"
+        self.label = f.read(16).rstrip(NULL)
+        self.connector, self.pin = unpack('BB', f.read(2)) # physical connector and pin
+        # max and min digital and analog values:
+        self.mindval, self.maxdval, self.minaval, self.maxaval = unpack('hhhh', f.read(8))
+        self.units = f.read(16).rstrip(NULL) # analog value units: "mV" or "uV"
+        ## TODO: given the corner freq values, perhaps the order of hi and lo fields
+        ## is reversed in the Blackrock documentation?
+        # hi corner freq: mHz; hi filt order: 0=None; hi filt type: 0=None, 1=Butterworth
+        self.hfcorner, self.hforder, self.hfilttype = unpack("IIH", f.read(10))
+        # lo corner freq: mHz; lo filt order: 0=None; lo filt type: 0=None, 1=Butterworth
+        self.lfcorner, self.lforder, self.lfilttype = unpack("IIH", f.read(10))
+
