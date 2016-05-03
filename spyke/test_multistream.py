a1d66bf9d63b0b6c3b54accf1221a31c62e775b0
Martin Spacek <git@mspacek.mm.st>
2016-05-03 22:52:15 +0200

    Rename test_trackstream.py -> test_multistream.py

--- spyke/test_multistream.py
+++ spyke/test_multistream.py
@@ -1,5 +1,5 @@
 from surf import File
-from core import TrackStream
+from core import MultiStream
 import numpy as np
 
 fnames = ['/data/ptc15/87 - track 7c spontaneous craziness.srf',
@@ -13,7 +13,7 @@ for fname in fnames:
     srff.parse()
     srffs.append(srff)
 
-ts = TrackStream(srffs=srffs)
+ts = MultiStream(srffs=srffs)
 ts.chans = np.array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49, 50, 51, 52, 53])
 t = ts.tranges
 
