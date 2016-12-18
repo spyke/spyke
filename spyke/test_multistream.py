b3a1b6dc20d46294542c9b8408cf595d225e0099
Martin Spacek <git@mspacek.mm.st>
2016-12-18 21:19:52 +0100

    Make test_multistream.py work again

--- spyke/test_multistream.py
+++ spyke/test_multistream.py
@@ -1,20 +1,25 @@
 from surf import File
-from core import MultiStream
+from stream import MultiStream
 import numpy as np
 
-fnames = ['/data/ptc15/87 - track 7c spontaneous craziness.srf',
-          '/data/ptc15/92 - track 7c mseq32 0.4deg.srf',
-          '/data/ptc15/93 - track 7c final CSD.srf']
+path = '~/data/slab/ptc15/'
+
+fnames = ['87 - track 7c spontaneous craziness.srf',
+          '92 - track 7c mseq32 0.4deg.srf',
+          '93 - track 7c final CSD.srf'
+          ]
 
 
-srffs = []
+fs = []
 for fname in fnames:
-    srff = File(fname)
-    srff.parse()
-    srffs.append(srff)
+    f = File(fname, path)
+    f.parse()
+    fs.append(f)
 
-ts = MultiStream(srffs=srffs)
-ts.chans = np.array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49, 50, 51, 52, 53])
+ts = MultiStream(fs, 'test.track')
+ts.chans = np.array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17,
+                      18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+                      37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49, 50, 51, 52, 53])
 t = ts.tranges
 
 waves = []
