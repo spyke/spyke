e8112a68ad59331c44765bc01b36b869ffbaa9a6
experiment <experiment@blab2>
2018-04-16 14:13:27 +0200

    Temporarily disable tight_layout for MPL 2.2, update TODO

--- spyke/sort.py
+++ spyke/sort.py
@@ -2988,5 +2988,6 @@ class SortWindow(SpykeToolWindow):
         # plot the smaller cluster last, to maximize visibility:
         for i in sortedmassis[::-1]:
             a.bar(ledges, hist[i], width=binwidth, color=cs[i], edgecolor=cs[i])
-        mplw.f.tight_layout(pad=0.3) # crop figure to contents
+        ## TODO: tight_layout call needs updating for MPL 2.2:
+        #mplw.f.tight_layout(pad=0.3) # crop figure to contents
         mplw.figurecanvas.draw()
