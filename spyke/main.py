eb95ec16095ed40ec04790edd451b44e50bd2aeb
Martin Spacek <git@mspacek.mm.st>
2020-02-12 16:30:28 +0100

    Fix int64 TypeError when exporting .envl.dat metadata to .json in Py3

--- spyke/main.py
+++ spyke/main.py
@@ -728,7 +728,7 @@ class SpykeWindow(QtGui.QMainWindow):
             enabledchans = self.hpstream.chans
             ypos = [ self.hpstream.probe.SiteLoc[chan][1] for chan in enabledchans ]
             ysortis = np.argsort(ypos)
-            ychans = list(enabledchans[ysortis])
+            ychans = enabledchans[ysortis].tolist() # of native Py int
             with open(fullfname, 'wb') as datf:
                 blocksize = int(float(self.ui.blockSizeLineEdit.text())) # allow exp notation
                 t0s = np.arange(hps.t0, hps.t1, blocksize)
@@ -738,7 +738,7 @@ class SpykeWindow(QtGui.QMainWindow):
                     wave = hps[t0xs:t1xs] # get excess range of data
                     data = wave.data[ysortis] # sort chans by depth
                     chans = wave.chans[ysortis]
-                    assert list(chans) == ychans
+                    assert chans.tolist() == ychans # both are lists of native Py int
                     if bipolarref:
                         # set each channel to be the difference of the two immediately
                         # spatially adjacent channels:
